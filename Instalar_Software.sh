0af07215afbb22cdecf779fab98e61d2ba92a9a8
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-09-26 17:57:18 +0200

    Plantear instalación de complementos para brackets y para atom

--- Instalar_Software.sh
+++ Instalar_Software.sh
@@ -23,7 +23,23 @@ verde="\033[1;32m"
 #############################
 ##   Variables Generales   ##
 #############################
-software=$(cat Software.lst)
+software=$(cat Software.lst) #Instala software del S.O.
+atom=$(cat Atom_Paquetes.lst) #Instala Paquetes de Atom
+
+#Instala complementos para Atom IDE
+function atom_install() {
+    echo -e "$verde Preparando instalación complementos Atom"
+    for p in $atom
+    do
+        echo -e "$verde Instalando $p"
+        apm install $p
+    done
+}
+
+#Instala complementos para Brackets IDE
+function brackets_install () {
+
+}
 
 #Recorrer "Software.lst" Instalando paquetes ahí descritos
 function instalar_Software() {
