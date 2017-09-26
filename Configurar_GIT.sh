72a7faeebb8810ebb4656d57773b5fe2b7737920
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-09-26 18:56:14 +0200

    Planteada entrada de datos en git

---
+++ Configurar_GIT.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+#######################################
+# ###     Raúl Caro Pastorino     ### #
+## ##                             ## ##
+### # https://github.com/fryntiz/ # ###
+## ##                             ## ##
+# ###       www.fryntiz.es        ### #
+#######################################
+
+############################
+##   Constantes Colores   ##
+############################
+amarillo="\033[1;33m"
+azul="\033[1;34m"
+blanco="\033[1;37m"
+cyan="\033[1;36m"
+gris="\033[0;37m"
+magenta="\033[1;35m"
+rojo="\033[1;31m"
+verde="\033[1;32m"
+
+#############################
+##   Variables Generales   ##
+#############################
+
+nombre_git=""
+usuario_git=""
+correo_git=""
+
+function configuracion_git() {
+    echo -e "$verde Configurando GIT$gris"
+    read -p "Introduce el nombre completo del programador → "
+    read -p "Introduce el usuario de GIT → "
+    read -p "Introduce el correo electronico → "
+}
