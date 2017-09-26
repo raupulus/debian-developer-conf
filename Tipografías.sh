094661bda7c9d4429eebffbc8b5a2eb94f98500b
fryntiz <tecnico@fryntiz.es>
2017-09-26 12:05:54 +0200

    Agregada función para descargar repositorio powerline

--- Tipografías.sh
+++ Tipografías.sh
@@ -24,7 +24,16 @@ verde="\033[1;32m"
 ##   Variables Generales   ##
 #############################
 
+#Agregar fuentes desde repositorio PowerLine
+function powerline() {
+	echo -e "$verde Clonando Repositorio$rojo PowerLine$amarillo"
+	git clone https://github.com/powerline/fonts.git ./fonts/
+	echo -e "$verde Se ha completado la descarga$gris"
+}
+
 function agregar_fuentes() {
+	powerline
+
 	echo "Añadiendo fuentes Tipográficas al sistema"
 	fuentes=$(ls fonts)
 	for f in $fuentes
