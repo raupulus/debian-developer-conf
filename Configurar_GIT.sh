2296dfb1af55d7be99f940b7450cbfd0dc77c6b5
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-09-26 19:24:57 +0200

    Completadas funciones

--- Configurar_GIT.sh
+++ Configurar_GIT.sh
@@ -28,9 +28,43 @@ nombre_git=""
 usuario_git=""
 correo_git=""
 
+function datos_input() {
+    read -p "Introduce el usuario de GITHUB → " usuario_git
+    read -p "Introduce el correo electronico → " correo_git
+}
+
+#Configurar el usuario GIT local
+function configurar_git() {
+    git config --global user.name $nombre_git
+    git config --global user.email $usuario_git
+}
+
+#Configura el usuario en GITHUB
+function configurar_github() {
+    git config --global github.name $usuario_git
+    composer config -g github-oauth.github.com
+}
+
 function configuracion_git() {
     echo -e "$verde Configurando GIT$gris"
-    read -p "Introduce el nombre completo del programador → "
-    read -p "Introduce el usuario de GIT → "
-    read -p "Introduce el correo electronico → "
+    read -p "Introduce el nombre completo del programador → " nombre_git
+
+    datos_input
+
+    while :
+    do
+        if [ -z usuario_git ] || [ -z correo_git ]
+        then
+            echo -e "$verde No puede estar vacio el usuario y el correo"
+            datos_input
+        else
+            break
+        fi
+    done
+
+    echo -e "$verde Configurando GIT local$gris"
+    configurar_git
+
+    echo -e "$verde Configurar conexion con GITHUB"
+    configurar_github
 }
