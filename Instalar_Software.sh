ceb69cb9e113edfa8afdb9c669afe21b2c590240
fryntiz <tecnico@fryntiz.es>
2017-09-27 15:48:50 +0200

    Añadidas instalaciones para atom, brackets, dbeaver, ninja-ide

--- Instalar_Software.sh
+++ Instalar_Software.sh
@@ -29,6 +29,12 @@ atom=$(cat Atom_Paquetes.lst) #Instala Paquetes de Atom
 #Instala complementos para Atom IDE
 function atom_install() {
     echo -e "$verde Instalando Atom $gris"
+	#Temporalmente se aplica descarga directa
+	wget https://atom.io/download/deb
+	mv deb atom.deb
+	sudo dpkg -i atom.deb
+	sudo apt install -f -y
+
     echo -e "$verde Preparando instalación complementos Atom$gris"
     for p in $atom
     do
@@ -40,6 +46,29 @@ function atom_install() {
 #Instala complementos para Brackets IDE
 function brackets_install () {
   	echo -e "$verde Preparando para instalar brackets$gris"
+	#Temporalmente se aplica descarga directa
+	wget https://github.com/adobe/brackets/releases/download/release-1.11/Brackets.Release.1.11.64-bit.deb
+	sudo dpkg -i Brackets.Release.1.11.64-bit.deb
+	sudo apt install -f
+}
+
+function dbeaver_install() {
+	echo -e "$verde Preparando instalacion para DBeaver"
+	#Temporalmente se aplica descarga directa
+	wget https://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb
+	sudo dpkg -i dbeaver-ce_latest_amd64.deb
+	sudo apt install -f
+}
+
+function ninjaide_install() {
+	echo -e "$verde Preparando instalacion para Ninja IDE"
+	wget http://ftp.es.debian.org/debian/pool/main/n/ninja-ide/ninja-ide_2.3-2_all.deb
+	sudo dpkg -i ninja-ide_2.3-2_all.deb
+	sudo apt install -f
+
+	#Resolviendo libreria QtWebKit.so
+	mkdir -p /usr/lib/python2.7/dist-packages/PyQt4/ 2>> /dev/null
+	cp ./LIB/usr/lib/python2.7/dist-packages/PyQt4/QtWebKit.so /usr/lib/python2.7/dist-packages/PyQt4/
 }
 
 #Recorrer "Software.lst" Instalando paquetes ahí descritos
@@ -54,9 +83,9 @@ function instalar_Software() {
 		sudo apt install -y $s
 	done
 
-	#Instalar Atom
+	#Instalaciones de software independiente
 	atom_install
-
-    #Instalar Brackets
     brackets_install
+	dbeaver_install
+	ninjaide_install
 }
