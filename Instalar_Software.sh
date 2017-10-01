ba074079168dc76de03d842b4cbfea0291123b05
fryntiz <tecnico@fryntiz.es>
2017-10-01 20:27:43 +0200

    Control de errores en la descarga para ninja-ide

--- Instalar_Software.sh
+++ Instalar_Software.sh
@@ -63,10 +63,11 @@ function brackets_install () {
 			wget https://github.com/adobe/brackets/releases/download/release-1.11/Brackets.Release.1.11.64-bit.deb
 		done
 		echo -e "$verde Preparando para instalar$rojo Brackets$gris"
-		sudo dpkg -i Brackets.Release.1.11.64-bit.deb && sudo apt install -f
+		sudo dpkg -i Brackets.Release.1.11.64-bit.deb && sudo apt install -f -y
 	fi
 }
 
+#Instala el editor de Base de Datos Dbeaver
 function dbeaver_install() {
 	if [ -f /usr/bin/dbeaver ]
 	then
@@ -79,20 +80,30 @@ function dbeaver_install() {
 			wget https://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb && break
 		done
 		echo -e "$verde Preparando para instalar$rojo Dbeaver$gris"
-		sudo dpkg -i dbeaver-ce_latest_amd64.deb && sudo apt install -f
+		sudo dpkg -i dbeaver-ce_latest_amd64.deb && sudo apt install -f -y
 	fi
 }
 
+#Instala el editor de python Ninja IDE
 function ninjaide_install() {
-	#TODO → Controlar errores en descarga como en atom y brackets
-	echo -e "$verde Preparando instalacion para Ninja IDE"
-	wget http://ftp.es.debian.org/debian/pool/main/n/ninja-ide/ninja-ide_2.3-2_all.deb
-	sudo dpkg -i ninja-ide_2.3-2_all.deb
-	sudo apt install -f
+	if [ -f /usr/bin/ninja-ide ]
+	then
+		echo -e "$verde Ya esta$rojo Ninja IDE$verde instalado en el equipo, omitiendo paso$gris"
+	else
+		REINTENTOS=3
+		echo -e "$verde Descargando$rojo Ninja IDE$gris"
+		for (( i=1; i<=$REINTENTOS; i++ ))
+		do
+			wget http://ftp.es.debian.org/debian/pool/main/n/ninja-ide/ninja-ide_2.3-2_all.deb && break
+		done
+		echo -e "$verde Preparando para instalar$rojo Ninja IDE$gris"
+		sudo dpkg -i ninja-ide_2.3-2_all.deb && sudo apt install -f -y
+	fi
 
-	#Resolviendo libreria QtWebKit.so si no existe
+	#Resolviendo dependencia de libreria QtWebKit.so si no existe
 	if [ ! -f /usr/lib/python2.7/dist-packages/PyQt4/QtWebKit.so ]
 	then
+		echo -e "$verde Añadiendo libreria$rojo QtWebKit$gris"
 		sudo mkdir -p /usr/lib/python2.7/dist-packages/PyQt4/ 2>> /dev/null
 		sudo cp ./LIB/usr/lib/python2.7/dist-packages/PyQt4/QtWebKit.so /usr/lib/python2.7/dist-packages/PyQt4/
 	fi
