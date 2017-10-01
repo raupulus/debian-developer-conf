70012b2e8b4c86adec5cc09947e0b9722d831532
fryntiz <tecnico@fryntiz.es>
2017-10-01 17:33:48 +0200

    Añadidos temas propios y otras configuraciones ademas de avisos

--- Instalar_Configuraciones.sh
+++ Instalar_Configuraciones.sh
@@ -35,13 +35,47 @@ function configurar_vim() {
 	echo -e "$verde Configurando VIM"
 
 	#Instalar Gestor de Plugins Vundle
-	echo -e "$verde Instalando gestor de plugins$rojo Vundle$gris"
+	echo -e "$verde Instalando gestor de plugins$rojo Vundle$gris" && sleep 2
 	if [ -f ~/.vim/bundle/Vundle.vim ]
 	then #Si existe solo actualiza plugins
 		echo | vim +PluginInstall +qall
-	else
-		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-		echo | vim +PluginInstall +qall #Instala plugins dentro de ~/.vimrc
+	else #Instala plugins dentro de ~/.vimrc #Se intenta 3 veces
+		for (i=0;i<3;i++)
+		do
+			git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+			echo | vim +PluginInstall +qall && break;
+		done
+	fi
+
+	#funcion para instalar todos los plugins
+	function vim_plugins() {
+		plugins_vim="powerline,youcompleteme,xmledit,autopep8,python-jedi,python-indent"
+		for plugin in $plugins_vim
+		do
+			echo -e "Activando el plugin  → $rojo $plugin$yellow" && sleep 2
+			vim-addon-manager install $plugin
+		done
+		echo -e "$verde Todos los plugins activados$gris"
+	}
+
+	function vim_colores() {
+		mkdir -p ~/.vim/colors 2>> /dev/null
+		#Creando archivos de colores, por defecto usara "monokai"
+		echo -e "$verde Descargando colores para sintaxis$amarillo"
+		if [ ! -f "~/.vim/colors/wombat.vim" ]
+		then
+			wget http://www.vim.org/scripts/download_script.php?src_id=6657 -O ~/.vim/colors/wombat.vim
+		fi
+
+		if [ ! -f "~/.vim/colors/monokai.vim" ]
+		then
+			wget https://raw.githubusercontent.com/lsdr/monokai/master/colors/monokai.vim -O ~/.vim/colors/monokai.vim
+		fi
+		echo -e "$verde Se ha concluido la instalacion de temas de colores$gris"
+	}
+
+	vim_plugins
+	vim_colores
 
 	#git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe
 	#cd ~/.vim/bundle/YouCompleteMe
@@ -59,11 +93,16 @@ function agregar_conf_home() {
 	do
 		if [ -f ~/$c ] || [ -d ~/$c ] #Si existe hago backup
 		then
-			echo -e "$verde Creando backup de ~/home/$(whoami)/$c $gris"
-			mv ~/$c ~/$c.BACKUP
+			if [ -f "~/$c.BACKUP" ] || [ -f "~/$c.BACKUP" ] #Contemplo que exista copia y no la borra
+			then
+				rm ~/$c 2>> /dev/null
+			else
+				echo -e "$verde Creando backup de ~/home/$(whoami)/$c $gris"
+				mv ~/$c ~/$c.BACKUP 2>> /dev/null
+			fi
 		fi
 		echo -e "$verde Generando configuración$gris"
-		cp -r ./home/$c ~/$c
+		cp -r ./home/$c ~/$c 2>> /dev/null
 	done
 }
 
@@ -83,16 +122,18 @@ function programas_default() {
 function terminal() {
   while true
   do
+  	echo -e "$verde 1) bash$gris"
+	echo -e "$verde 2) zsh$gris"
   	read -p "Introduce el terminal → bash/zsh: " term
   	case $term in
-		bash)#Establecer bash como terminal
+		bash || 1)#Establecer bash como terminal
 			chsh -s /bin/bash
 			break;;
-		zsh)#Establecer zsh como terminal
+		zsh || 2)#Establecer zsh como terminal
 			chsh -s /bin/zsh
 			break;;
 		*)#Opción errónea
-			echo -e "$rojo Opción no válida"
+			echo -e "$rojo Opción no válida$gris"
 	esac
   done
 }
