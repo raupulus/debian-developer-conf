07047dc692106201cb356cfd49f48bfe6f819109
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-04 00:36:05 +0200

    Planteada estructura para instalar, personalizar y configurar servidores

--- Servidores.sh
+++ Servidores.sh
@@ -23,3 +23,68 @@ verde="\033[1;32m"
 #############################
 ##   Variables Generales   ##
 #############################
+
+function server_apache() {
+
+    function instalar_apache() {
+    echo -e "$verde Instalando Apache 2$gris"
+    }
+
+    function configurar_apache() {
+    echo -e "$verde Preparando configuracion de Apache 2$gris"
+    }
+
+    function personalizar_apache() {
+    echo -e "$verde Personalizando Apache 2$gris"
+    }
+
+    instalar_apache
+    configurar_apache
+    personalizar_apache
+}
+
+function server_php() {
+
+    function instalar_php() {
+    echo -e "$verde Instalando Apache 2$gris"
+    }
+
+    function configurar_php() {
+    echo -e "$verde Preparando configuracion de Apache 2$gris"
+    }
+
+    function personalizar_php() {
+    echo -e "$verde Personalizando Apache 2$gris"
+    }
+
+    instalar_php
+    configurar_php
+    personalizar_php
+}
+
+function server_sql() {
+
+    function instalar_sql() {
+    echo -e "$verde Instalando Apache 2$gris"
+    }
+
+    function configurar_sql() {
+    echo -e "$verde Preparando configuracion de Apache 2$gris"
+    }
+
+    function personalizar_sql() {
+    echo -e "$verde Personalizando Apache 2$gris"
+    }
+
+    instalar_sql
+    configurar_sql
+    personalizar_sql
+}
+
+function instalar_servidores() {
+    server_apache
+    server_php
+    server_sql
+
+    echo -e "$verde Se ha completado la instalacion, configuracion y personalizacion de servidores"
+}
