64cc750a2f94fabfcd641808139def2cb4ac6c17
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-04 00:13:03 +0200

    Planteado metodo de personalizacion gtk

--- Personalización_GTK.sh
+++ Personalización_GTK.sh
@@ -23,3 +23,27 @@ verde="\033[1;32m"
 #############################
 ##   Variables Generales   ##
 #############################
+
+function configurar_iconos(){
+    echo -e "$verde Configurando pack de iconos$gris"
+}
+
+function configurar_cursores(){
+    echo -e "$verde Configurando pack de cursores$gris"
+}
+
+function configurar_grub() {
+    echo -e "$verde Configurando fondo del grub$gris"
+}
+
+function configurar_fondos {
+    echo -e "$verde Configurando fondo de pantalla$gris"
+}
+
+function personalizar() {
+    echo -e "$verde Iniciando configuracion de estetica general y GTK$gris"
+    configurar_iconos
+    configurar_cursores
+    configurar_grub
+    configurar_fondos
+}
