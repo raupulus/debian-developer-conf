af96f90f983d12efbe8ccc1699f862523c025718
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-11 01:42:55 +0200

    Reparadas tabulaciones/espacios(4)

--- Instalar_Software.sh
+++ Instalar_Software.sh
@@ -28,20 +28,20 @@ atom=$(cat Atom_Paquetes.lst) #Instala Paquetes de Atom
 
 #Instala complementos para Atom IDE
 function atom_install() {
-	if [ -f /usr/bin/atom ]
-	then
-		echo -e "$verde Ya esta$rojo ATOM$verde instalado en el equipo, omitiendo paso$gris"
-	else
-		REINTENTOS=3
-		echo -e "$verde Descargando$rojo ATOM$gris"
-		for (( i=1; i<=$REINTENTOS; i++ ))
-		do
-			rm deb atom.deb 2>> /dev/null
-			wget https://atom.io/download/deb && mv deb atom.deb && break
-		done
-		echo -e "$verde Instalando$rojo Atom $gris"
-		sudo dpkg -i atom.deb && sudo apt install -f -y
-	fi
+    if [ -f /usr/bin/atom ]
+    then
+        echo -e "$verde Ya esta$rojo ATOM$verde instalado en el equipo, omitiendo paso$gris"
+    else
+        REINTENTOS=3
+        echo -e "$verde Descargando$rojo ATOM$gris"
+        for (( i=1; i<=$REINTENTOS; i++ ))
+        do
+            rm deb atom.deb 2>> /dev/null
+            wget https://atom.io/download/deb && mv deb atom.deb && break
+        done
+        echo -e "$verde Instalando$rojo Atom $gris"
+        sudo dpkg -i atom.deb && sudo apt install -f -y
+    fi
 
     echo -e "$verde Preparando instalación complementos$rojo Atom$gris"
     for p in $atom
@@ -53,94 +53,93 @@ function atom_install() {
 
 #Instala complementos para Brackets IDE
 function brackets_install () {
-	if [ -f /usr/bin/brackets ]
-	then
-		echo -e "$verde Ya esta$rojo Brackets$verde instalado en el equipo, omitiendo paso$gris"
-	else
-		REINTENTOS=10
-		echo -e "$verde Descargando$rojo Brackets$gris"
-		for (( i=1; i<=$REINTENTOS; i++ ))
-		do
-			rm Brackets.Release.1.10.64-bit.deb 2>> /dev/null
-			wget https://github.com/adobe/brackets/releases/download/release-1.10/Brackets.Release.1.10.64-bit.deb && break
-		done
+    if [ -f /usr/bin/brackets ]
+    then
+        echo -e "$verde Ya esta$rojo Brackets$verde instalado en el equipo, omitiendo paso$gris"
+    else
+        REINTENTOS=10
+        echo -e "$verde Descargando$rojo Brackets$gris"
+        for (( i=1; i<=$REINTENTOS; i++ ))
+        do
+            rm Brackets.Release.1.10.64-bit.deb 2>> /dev/null
+            wget https://github.com/adobe/brackets/releases/download/release-1.10/Brackets.Release.1.10.64-bit.deb && break
+        done
 
         for (( i=1; i<=$REINTENTOS; i++ ))
-		do
-			rm libgcrypt11_1.5.0-5+deb7u6_amd64.deb 2>> /dev/null
-			wget http://security.debian.org/debian-security/pool/updates/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u6_amd64.deb && break
-		done
+        do
+            rm libgcrypt11_1.5.0-5+deb7u6_amd64.deb 2>> /dev/null
+            wget http://security.debian.org/debian-security/pool/updates/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u6_amd64.deb && break
+        done
 
-		echo -e "$verde Preparando para instalar$rojo Brackets$gris"
-		sudo dpkg -i libgcrypt11_1.5.0-5+deb7u6_amd64.deb
-		sudo dpkg -i Brackets.Release.1.10.64-bit.deb && sudo apt install -f -y
-	fi
+        echo -e "$verde Preparando para instalar$rojo Brackets$gris"
+        sudo dpkg -i libgcrypt11_1.5.0-5+deb7u6_amd64.deb
+        sudo dpkg -i Brackets.Release.1.10.64-bit.deb && sudo apt install -f -y
+    fi
 }
 
 #Instala el editor de Base de Datos Dbeaver
 function dbeaver_install() {
-	if [ -f /usr/bin/dbeaver ]
-	then
-		echo -e "$verde Ya esta$rojo Dbeaver$verde instalado en el equipo, omitiendo paso$gris"
-	else
-		REINTENTOS=3
-		echo -e "$verde Descargando$rojo Dbeaver$gris"
-		for (( i=1; i<=$REINTENTOS; i++ ))
-		do
-			rm dbeaver-ce_latest_amd64.deb 2>> /dev/null
-			wget https://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb && break
-		done
-		echo -e "$verde Preparando para instalar$rojo Dbeaver$gris"
-		sudo dpkg -i dbeaver-ce_latest_amd64.deb && sudo apt install -f -y
-	fi
+    if [ -f /usr/bin/dbeaver ]
+    then
+        echo -e "$verde Ya esta$rojo Dbeaver$verde instalado en el equipo, omitiendo paso$gris"
+    else
+        REINTENTOS=3
+        echo -e "$verde Descargando$rojo Dbeaver$gris"
+        for (( i=1; i<=$REINTENTOS; i++ ))
+        do
+            rm dbeaver-ce_latest_amd64.deb 2>> /dev/null
+            wget https://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb && break
+        done
+        echo -e "$verde Preparando para instalar$rojo Dbeaver$gris"
+        sudo dpkg -i dbeaver-ce_latest_amd64.deb && sudo apt install -f -y
+    fi
 }
 
 #Instala el editor de python Ninja IDE
 function ninjaide_install() {
-	if [ -f /usr/bin/ninja-ide ]
-	then
-		echo -e "$verde Ya esta$rojo Ninja IDE$verde instalado en el equipo, omitiendo paso$gris"
-	else
-		REINTENTOS=3
-		echo -e "$verde Descargando$rojo Ninja IDE$gris"
-		for (( i=1; i<=$REINTENTOS; i++ ))
-		do
-			rm ninja-ide_2.3-2_all.deb 2>> /dev/null
-			wget http://ftp.es.debian.org/debian/pool/main/n/ninja-ide/ninja-ide_2.3-2_all.deb && break
-		done
-		echo -e "$verde Preparando para instalar$rojo Ninja IDE$gris"
-		sudo dpkg -i ninja-ide_2.3-2_all.deb && sudo apt install -f -y
-	fi
+    if [ -f /usr/bin/ninja-ide ]
+    then
+        echo -e "$verde Ya esta$rojo Ninja IDE$verde instalado en el equipo, omitiendo paso$gris"
+    else
+        REINTENTOS=3
+        echo -e "$verde Descargando$rojo Ninja IDE$gris"
+        for (( i=1; i<=$REINTENTOS; i++ ))
+        do
+            rm ninja-ide_2.3-2_all.deb 2>> /dev/null
+            wget http://ftp.es.debian.org/debian/pool/main/n/ninja-ide/ninja-ide_2.3-2_all.deb && break
+        done
+        echo -e "$verde Preparando para instalar$rojo Ninja IDE$gris"
+        sudo dpkg -i ninja-ide_2.3-2_all.deb && sudo apt install -f -y
+    fi
 
-	#Resolviendo dependencia de libreria QtWebKit.so si no existe
-	if [ ! -f /usr/lib/python2.7/dist-packages/PyQt4/QtWebKit.so ]
-	then
-		echo -e "$verde Añadiendo libreria$rojo QtWebKit$gris"
-		sudo mkdir -p /usr/lib/python2.7/dist-packages/PyQt4/ 2>> /dev/null
-		sudo cp ./LIB/usr/lib/python2.7/dist-packages/PyQt4/QtWebKit.so /usr/lib/python2.7/dist-packages/PyQt4/
-	fi
+    #Resolviendo dependencia de libreria QtWebKit.so si no existe
+    if [ ! -f /usr/lib/python2.7/dist-packages/PyQt4/QtWebKit.so ]
+    then
+        echo -e "$verde Añadiendo libreria$rojo QtWebKit$gris"
+        sudo mkdir -p /usr/lib/python2.7/dist-packages/PyQt4/ 2>> /dev/null
+        sudo cp ./LIB/usr/lib/python2.7/dist-packages/PyQt4/QtWebKit.so /usr/lib/python2.7/dist-packages/PyQt4/
+    fi
 }
 
 #Recorrer "Software.lst" Instalando paquetes ahí descritos
 function instalar_Software() {
-	echo -e "$verde Actualizando listas de$rojo Repositorios$verde (Paciencia)$gris"
-	sudo apt update >> /dev/null 2>> /dev/null
+    echo -e "$verde Actualizando listas de$rojo Repositorios$verde (Paciencia)$gris"
+    sudo apt update >> /dev/null 2>> /dev/null
     echo -e "$verde Comprobando estado del$rojo Gestor de paquetes$verde (Paciencia)$gris"
-	sudo apt --fix-broken install 2>> /dev/null
-	sudo apt install -f -y 2>> /dev/null
-	echo -e "$verde Instalando Software adicional$gris"
-	for s in $software
-	do
-		echo -e "$verde Preparando para instalar $rojo$s$gris"
-		sudo apt install -y $s >> /dev/null 2>> /dev/null && echo -e "$rojo $s$verde instalado correctamente" || echo -e "$rojo $s$verde No se ha instalado"
-	done
+    sudo apt --fix-broken install 2>> /dev/null
+    sudo apt install -f -y 2>> /dev/null
+    echo -e "$verde Instalando Software adicional$gris"
+    for s in $software
+    do
+        sudo apt install -y $s >> /dev/null 2>> /dev/null && echo -e "$rojo $s$verde instalado correctamente" || echo -e "$rojo $s$verde No se ha instalado"
+    done
 
-	#Instalaciones de software independiente
-	atom_install
+    #Instalaciones de software independiente
+    atom_install
     brackets_install
-	dbeaver_install
-	ninjaide_install
+    dbeaver_install
+    ninjaide_install
 
-	sudo apt --fix-broken install 2>> /dev/null
-	sudo apt install -f -y 2>> /dev/null
+    sudo apt --fix-broken install 2>> /dev/null
+    sudo apt install -f -y 2>> /dev/null
 }
