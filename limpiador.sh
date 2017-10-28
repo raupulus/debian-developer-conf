294f249780b89610c7864bc5554407aee65bacce
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-28 20:40:24 +0200

    Creado script para limpiar configuraciones de manera agresiva

--- limpiador.sh
+++ limpiador.sh
@@ -28,3 +28,37 @@ verde="\033[1;32m"
 ##   Variables Generales   ##
 #############################
 
+function limpiar_con_fuerza() {
+    echo -e "$verde Este script es muy peligroso y borrara muchas cosas"
+    echo -e "$verde Asegúrate que es lo que necesitas hacer antes de ejecutarlo"
+    read -p "Pulsa una tecla para destruir personalización generada por este script"
+    rm -R -f ~/.oh-my-zsh 2>> /dev/null
+    rm -R -f ~/.bash_it 2>> /dev/null
+    rm -R -f ~/.nvm 2>> /dev/null
+    rm -R -f ~/fasd 2>> /dev/null
+    rm -R -f ~/.vim 2>> /dev/null
+    rm -R -f ~/.local/opt/Firefox_Quantum_Developer 2>> /dev/null
+    rm -R -f ~/.local/opt/Firefox_Nightly 2>> /dev/null
+    rm -R -f ~/.atom /dev/null
+    rm -R -f ~/.i3 /dev/null
+    rm -R -f ~/.ninja-ide /dev/null
+    rm -R -f ~/dunst /dev/null
+    rm -R -f ~/powerline /dev/null
+    rm -R -f ~/sakura /dev/null
+    rm -R -f ~/.less /dev/null
+    rm -R -f ~/.lessfilter /dev/null
+    rm -R -f ~/.tmux.conf /dev/null
+    rm -R -f ~/.tmux.conf2 /dev/null
+    rm -R -f ~/.udisks-glue.conf /dev/null
+    rm -R -f ~/.vimrc /dev/null
+    rm -R -f ~/.zshrc /dev/null
+}
+limpiar_con_fuerza
+
+
+
+
+
+
+
+
