7162682c6cbb0eb8cd75455683f9e5bb500f0ec5
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-31 19:37:47 +0100

    Fix set terminal

--- Instalar_Configuraciones.sh
+++ Instalar_Configuraciones.sh
@@ -220,7 +220,7 @@ function programas_default() {
         #sudo update-alternatives --install /usr/bin/tilix x-terminal-emulator /usr/bin/tilix 1
         sudo touch /etc/profile.d/vte.sh 2>> /dev/null
         #TOFIX → No existe Tilix como alternativa, reparar primer paso
-        sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix 2>> /dev/null
+        sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix.wrapper 2>> /dev/null
     elif [ -f /usr/bin/terminator ]
     then
         echo -e "$verde Estableciendo terminal por defecto a$rojo Terminator$gris"
