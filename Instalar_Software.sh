1f845df6f22e5bc96aea5b2b73f3441a51fac253
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-03 01:13:04 +0100

    Extraido atom del script

--- Instalar_Software.sh
+++ Instalar_Software.sh
@@ -8,75 +8,12 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
-############################
-##   Constantes Colores   ##
-############################
-amarillo="\033[1;33m"
-azul="\033[1;34m"
-blanco="\033[1;37m"
-cyan="\033[1;36m"
-gris="\033[0;37m"
-magenta="\033[1;35m"
-rojo="\033[1;31m"
-verde="\033[1;32m"
-
 #############################
 ##   Variables Generales   ##
 #############################
 software=$(cat Software.lst) #Instala software del S.O.
-atom=$(cat Atom_Paquetes.lst) #Instala Paquetes de Atom
 
-#Instala complementos para Atom IDE
-function atom_install() {
-    if [ -f /usr/bin/atom ]
-    then
-        echo -e "$verde Ya esta$rojo ATOM$verde instalado en el equipo, omitiendo paso$gris"
-    else
-        REINTENTOS=3
-        echo -e "$verde Descargando$rojo ATOM$gris"
-        for (( i=1; i<=$REINTENTOS; i++ ))
-        do
-            rm deb atom.deb 2>> /dev/null
-            wget https://atom.io/download/deb && mv deb atom.deb && break
-        done
-        echo -e "$verde Instalando$rojo Atom $gris"
-        sudo dpkg -i atom.deb && sudo apt install -f -y
-    fi
-
-    # Si se ha instalado correctamente ATOM pues instalamos sus plugins
-    echo -e "$verde Preparando instalación complementos$rojo Atom$gris"
-    if [ -f /usr/bin/atom ]
-    then
-        for p in $atom
-        do
-            # Comprobación si existe instalado el complemento
-            if [ -d "$HOME/.atom/packages/$p" ]
-            then
-                echo -e "$amarillo Ya se encuentra instalado →$rojo $p"
-            else
-                echo -e "$verde Instalando$rojo $p $amarillo"
-                apm install $p
-            fi
-        done
-    fi
-
-    function conf_atom() {
-        echo -e "$verde Añadiendo configuraciones para Atom"
-
-        echo -e "$verde Deshabilitando complementos"
-        apm disable welcome
-        apm disable about
-
-        # TODO → Añadir configuración de idiomas
-        # echo '
-        # "atom-i18n":
-        #   customMenuI18nPath: "$HOME/.atom/packages/atom-i18n/def/custom_menu.cson"
-        #   locale: "es"
-        # '
-    }
-
-    conf_atom
-}
+source $DIR_SCRIPT/Apps/Atom_IDE.sh
 
 #Instala complementos para Brackets IDE
 function brackets_install() {
