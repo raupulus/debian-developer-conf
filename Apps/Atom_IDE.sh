54a13e3c7b9fd64edf90ccba11f2a834235390a7
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-03 01:54:08 +0100

    Añadido coloreado

--- Apps/Atom_IDE.sh
+++ Apps/Atom_IDE.sh
@@ -8,15 +8,29 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
+############################
+##   Constantes Colores   ##
+############################
+amarillo="\033[1;33m"
+azul="\033[1;34m"
+blanco="\033[1;37m"
+cyan="\033[1;36m"
+gris="\033[0;37m"
+magenta="\033[1;35m"
+rojo="\033[1;31m"
+verde="\033[1;32m"
+
 #############################
 ##   Variables Generales   ##
 #############################
-atom=$(cat $DIR_SCRIPT/Apps/Atom_Paquetes.lst) #Instala Paquetes de Atom
+atom=$(cat $PWD/Apps/Atom_Paquetes.lst) #Instala Paquetes de Atom
 
 function atom_preconfiguracion() {
-    echo -e "$verde Añadir configuración$amarillo"
+    echo ""
+    echo -e "$verde Se va a instalar$rojo Atom IDE$gris"
+    echo -e "$verde Puedes añadir configuraciones$amarillo"
     read -p '¿Quieres configuraciones? s/N → ' input
-    if [ $input == s ] || [ $input == S]
+    if [ $input == s ] || [ $input == S ]
     then
         mv $HOME/.atom $HOME/.atom.BACKUP
         cp .atom $HOME/
--- Instalar_Software.sh
+++ Instalar_Software.sh
@@ -8,12 +8,24 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
+############################
+##   Constantes Colores   ##
+############################
+amarillo="\033[1;33m"
+azul="\033[1;34m"
+blanco="\033[1;37m"
+cyan="\033[1;36m"
+gris="\033[0;37m"
+magenta="\033[1;35m"
+rojo="\033[1;31m"
+verde="\033[1;32m"
+
 #############################
 ##   Variables Generales   ##
 #############################
 software=$(cat Software.lst) #Instala software del S.O.
 
-source $DIR_SCRIPT/Apps/Atom_IDE.sh
+source Apps/Atom_IDE.sh
 
 #Instala complementos para Brackets IDE
 function brackets_install() {
