d40bf8dcb0d5d72d0c7b79eb7f064f7719c1d1f8
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-04 01:50:33 +0100

    Limpieza de código

--- Apps/Firefox.sh
+++ Apps/Firefox.sh
@@ -49,19 +49,11 @@ function firefox_install() {
 
         function instalar() {
             # Desempaquetar Firefox-Quantum_amd64.tar.bz2
-            mkdir $DIR_SCRIPT/TMP/Firefox-Quantum-Developer_amd64 2>> /dev/null
-            tar -xjvf $DIR_SCRIPT/TMP/Firefox-Quantum-Developer_amd64.tar.bz2 -C $DIR_SCRIPT/TMP/Firefox-Quantum-Developer_amd64
+            mkdir -p $DIR_SCRIPT/TMP/Firefox-Quantum-Developer_amd64 2>> /dev/null
+            tar -xjvf $DIR_SCRIPT/TMP/Firefox-Quantum-Developer_amd64.tar.bz2 -C $DIR_SCRIPT/TMP/Firefox-Quantum-Developer_amd64 2>> /dev/null
 
             # Mover archivo extraido a su ubicación final
-            mv $DIR_SCRIPT/TMP/Firefox-Quantum-Developer_amd64/firefox ~/.local/opt/Firefox_Quantum_Developer
-
-            # Desempaquetar Perfil y moverlo
-            # if [ ! -d ~/.mozilla/firefox/i5727yjx.Firefox-Quantum ]
-            # then
-            #     mkdir $DIR_SCRIPT/Apps/Perfiles_Firefox/i5727yjx.Firefox-Quantum 2>> /dev/null
-            #     tar -xJvf $DIR_SCRIPT/Apps/Perfiles_Firefox/i5727yjx.Firefox-Quantum.tar.xz -C $DIR_SCRIPT/Apps/Perfiles_Firefox/i5727yjx.Firefox-Quantum 2>> /dev/null
-            #     mv $DIR_SCRIPT/Apps/Perfiles_Firefox/i5727yjx.Firefox-Quantum/* ~/.mozilla/firefox/ 2>> /dev/null
-            # fi
+            mv $DIR_SCRIPT/TMP/Firefox-Quantum-Developer_amd64/firefox ~/.local/opt/Firefox_Quantum_Developer 2>> /dev/null
 
             # Crear enlaces de usuario y permisos de ejecución
             echo "$HOME/.local/opt/Firefox_Quantum_Developer/firefox - P Firefox-Quantum" > ~/.local/bin/firefox-quantum
@@ -70,19 +62,6 @@ function firefox_install() {
 
             # Copiar acceso directo
             cp Accesos_Directos/firefox-quantum.desktop ~/.local/share/applications/ 2>> /dev/null
-
-            # Pedir crear perfil
-            # echo -e "$verde Para evitar conflictos entre distintas versiones crea un perfil"
-            # echo -e "$verde Al pulsar una tecla se abrirá una ventana para ello"
-            # echo -e "$verde El nombre convendrá que sea lógico como →$rojo Firefox-Quantum$amarillo"
-            # read -p "Pulsa una tecla para abrir el ProfileManager" x
-            # ~/.local/bin/firefox-quantum --ProfileManager
-
-            if [ -f ~/.local/bin/firefox-quantum ]
-            then
-                # Crear perfil para Firefox-Quantum
-                firefox-quantum -createprofile Firefox-Quantum
-            fi
         }
 
         if [ -f ~/.local/bin/firefox-quantum ]
@@ -111,40 +90,26 @@ function firefox_install() {
 
         function instalar() {
              # Desempaquetar Firefox-Nightly_amd64.tar.bz2
-            mkdir $DIR_SCRIPT/TMP/Firefox-Nightly_amd64 2>> /dev/null
-            tar -xjvf $DIR_SCRIPT/TMP/Firefox-Nightly_amd64.tar.bz2 -C $DIR_SCRIPT/TMP/Firefox-Nightly_amd64
+            mkdir -p $DIR_SCRIPT/TMP/Firefox-Nightly_amd64 2>> /dev/null
+            tar -xjvf $DIR_SCRIPT/TMP/Firefox-Nightly_amd64.tar.bz2 -C $DIR_SCRIPT/TMP/Firefox-Nightly_amd64 2>> /dev/null
 
             # Mover archivo extraido a su ubicación final
-            mv $DIR_SCRIPT/TMP/Firefox-Nightly_amd64/firefox ~/.local/opt/Firefox_Nightly
-
-            # Desempaquetar Perfil y moverlo
-            # if [ ! -d ~/.mozilla/firefox/fw493le3.Firefox-Nightly ]
-            # then
-            #     mkdir $DIR_SCRIPT/Apps/Perfiles_Firefox/fw493le3.Firefox-Nightly 2>> /dev/null
-            #     tar -xJvf $DIR_SCRIPT/Apps/Perfiles_Firefox/fw493le3.Firefox-Nightly.tar.xz -C $DIR_SCRIPT/Apps/Perfiles_Firefox/fw493le3.Firefox-Nightly 2>> /dev/null
-            #     mv $DIR_SCRIPT/Apps/Perfiles_Firefox/fw493le3.Firefox-Nightly/* ~/.mozilla/firefox 2>> /dev/null
-            # fi
+            mv $DIR_SCRIPT/TMP/Firefox-Nightly_amd64/firefox ~/.local/opt/Firefox_Nightly 2>> /dev/null
 
             # Crear enlaces de usuario y permisos de ejecución
             echo "~/.local/opt/Firefox_Nightly/firefox - P Firefox-Nightly" > ~/.local/bin/firefox-nightly
-            # ln -s ~/.local/opt/Firefox_Nightly/firefox ~/.local/bin/firefox-nightly 2>> /dev/null
-
             chmod +x ~/.local/bin/firefox-nightly 2>> /dev/null
 
-            # Copiar acceso directo
+            # Acceso directo Desktop
             cp -R Accesos_Directos/firefox-nightly.desktop ~/.local/share/applications/
 
-            # Pedir crear perfil
-            # echo -e "$verde Para evitar conflictos entre distintas versiones crea un perfil"
-            # echo -e "$verde Al pulsar una tecla se abrirá una ventana para ello"
-            # echo -e "$verde El nombre convendrá que sea lógico como →$rojo Firefox-Nightly$amarillo"
-            # read -p "Pulsa una tecla para abrir el ProfileManager" x
-            # ~/.local/bin/firefox-nightly --ProfileManager
-
-            if [ -f ~/.local/bin/firefox-nightly ]
+            # Crear Perfil para Firefox
+            if [ -f ~/.local/opt/firefox-nightly/firefox ]
             then
+                # Crear perfil para Firefox-Quantum
+                ~/.local/opt/Firefox_Nightly/firefox -createprofile Firefox-Quantum
                 # Crear perfil para Firefox-Nightly
-                firefox-nightly -createprofile Firefox-Nightly
+                ~/.local/opt/Firefox_Nightly/firefox -createprofile Firefox-Nightly
             fi
         }
 
