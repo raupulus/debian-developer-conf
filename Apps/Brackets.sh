4d9dce0f0e07acae85e1e620b013520371880e8d
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-24 18:14:12 +0100

    Corregidas rutas

--- Apps/Brackets.sh
+++ Apps/Brackets.sh
@@ -31,14 +31,14 @@ function brackets_descargar() {
     REINTENTOS=10
     for (( i=1; i<=$REINTENTOS; i++ ))
     do
-        rm Brackets.Release.1.10.64-bit.deb 2>> /dev/null
-        wget https://github.com/adobe/brackets/releases/download/release-1.10/Brackets.Release.1.10.64-bit.deb && break
+        rm $DIR_SCRIPT/TMP/Brackets.Release.1.10.64-bit.deb 2>> /dev/null
+        wget https://github.com/adobe/brackets/releases/download/release-1.10/Brackets.Release.1.10.64-bit.deb -O $DIR_SCRIPT/TMP/Brackets.Release.1.10.64-bit.deb && break
     done
 
     for (( i=1; i<=$REINTENTOS; i++ ))
     do
         rm libgcrypt11_1.5.0-5+deb7u6_amd64.deb 2>> /dev/null
-        wget http://security.debian.org/debian-security/pool/updates/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u6_amd64.deb && break
+        wget http://security.debian.org/debian-security/pool/updates/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u6_amd64.deb -O $DIR_SCRIPT/TMP/libgcrypt11_1.5.0-5+deb7u6_amd64.deb && break
     done
 }
 
@@ -48,10 +48,10 @@ function brackets_preconfiguracion() {
 
 function brackets_instalar() {
     echo -e "$verde Preparando para instalar$rojo Brackets$gris"
-    sudo dpkg -i libgcrypt11_1.5.0-5+deb7u6_amd64.deb
+    sudo dpkg -i $DIR_SCRIPT/TMP/libgcrypt11_1.5.0-5+deb7u6_amd64.deb
 
     echo -e "$verde Instalando$rojo Brackets$gris"
-    sudo dpkg -i Brackets.Release.1.10.64-bit.deb && sudo apt install -f -y
+    sudo dpkg -i $DIR_SCRIPT/TMP/Brackets.Release.1.10.64-bit.deb && sudo apt install -f -y
 }
 
 function brackets_postconfiguracion() {
