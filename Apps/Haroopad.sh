3577dc701c2ef2bb105ae8a3e8089ef35fdd721b
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-24 18:46:38 +0100

    Refactorizada instalación de Haroopad

--- Apps/Haroopad.sh
+++ Apps/Haroopad.sh
@@ -23,9 +23,16 @@ verde="\033[1;32m"
 #############################
 ##   Variables Generales   ##
 #############################
+DIR_SCRIPT=`echo $PWD`
 
 function haroopad_descargar() {
     echo -e "$verde Descargando$rojo Haroopad$gris"
+    REINTENTOS=10
+    for (( i=1; i<=$REINTENTOS; i++ ))
+    do
+        rm $DIR_SCRIPT/TMP/haroopad-v0.13.1-x64.deb 2>> /dev/null
+        wget --show-progress https://bitbucket.org/rhiokim/haroopad-download/downloads/haroopad-v0.13.1-x64.deb -O $DIR_SCRIPT/TMP/haroopad-v0.13.1-x64.deb&& break
+    done
 }
 
 function haroopad_preconfiguracion() {
@@ -34,6 +41,7 @@ function haroopad_preconfiguracion() {
 
 function haroopad_instalar() {
     echo -e "$verde Instalando$rojo Haroopad$gris"
+    sudo dpkg -i $DIR_SCRIPT/TMP/haroopad-v0.13.1-x64.deb && sudo apt install -f -y
 }
 
 function haroopad_postconfiguracion() {
@@ -42,4 +50,29 @@ function haroopad_postconfiguracion() {
 
 function haroopad_instalador() {
     echo -e "$verde Comenzando instalación de$rojo Haroopad$gris"
+
+    haroopad_preconfiguracion
+
+    if [ -f /usr/bin/haroopad ]
+    then
+        echo -e "$verde Ya esta$rojo Haroopad$verde instalado en el equipo, omitiendo paso$gris"
+    else
+        if [ -f $DIR_SCRIPT/TMP/haroopad-v0.13.1-x64.deb ]
+        then
+            brackets_instalar
+        else
+            haroopad_descargar
+            haroopad_instalar
+        fi
+
+        # Si falla la instalación se rellama la función tras limpiar
+        if [ ! -f /usr/bin/haroopad ]
+        then
+            rm -f $DIR_SCRIPT/TMP/haroopad-v0.13.1-x64.deb
+            haroopad_descargar
+            haroopad_instalar
+        fi
+    fi
+
+    haroopad_postconfiguracion
 }
--- Instalar_Software_Extra.sh
+++ Instalar_Software_Extra.sh
@@ -31,23 +31,6 @@ source Apps/Ninja-IDE.sh
 source Apps/Brackets.sh
 source Apps/DBeaver.sh
 
-function haroopad_install() {
-    if [ -f /usr/bin/haroopad ]
-    then
-        echo -e "$verde Ya esta$rojo Haroopad$verde instalado en el equipo, omitiendo paso$gris"
-    else
-        REINTENTOS=3
-        echo -e "$verde Descargando$rojo Haroopad$gris"
-        for (( i=1; i<=$REINTENTOS; i++ ))
-        do
-            rm haroopad-v0.13.1-x64.deb 2>> /dev/null
-            wget --show-progress https://bitbucket.org/rhiokim/haroopad-download/downloads/haroopad-v0.13.1-x64.deb && break
-        done
-        echo -e "$verde Preparando para instalar$rojo Haroopad$gris"
-        sudo dpkg -i haroopad-v0.13.1-x64.deb && sudo apt install -f -y
-    fi
-}
-
 function instalar_Software_Extra() {
     echo -e "$verde Preparando para instalar software Extra$gris"
     atom_instalador
