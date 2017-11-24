687e4b0fa6878ba681ced50f1e7b565adad164c5
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-24 13:04:02 +0100

    Añadidas plantillas

---
+++ Plantillas/Web/Javascript.js
@@ -0,0 +1,6 @@
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+
---
+++ Plantillas/Web/PHP.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+?>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <meta name="description" content="Descripción"/>
+        <meta name="keywords" content="Raúl Caro Pastorino, Fryntiz"/>
+        <meta name="author" content="Raúl Caro Pastorino"/>
+        <link rel="shortcut icon" href="./images/favicon.png"/>
+        <link rel="stylesheet" href="./CSS/styles.css"/>
+        <script src="JS/scripts.js"></script>
+    </head>
+
+    <body>
+
+    </body>
+</html>
+
