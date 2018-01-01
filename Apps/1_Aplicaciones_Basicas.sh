29d21b606f9af47e3e5f396982a70d62e11ad055
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-01 01:43:21 +0100

    Agregada sección que agrupa aplicaciones Básicas

---
+++ Apps/1_Aplicaciones_Basicas.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2017 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+
+############################
+##     IMPORTACIONES      ##
+############################
+
+############################
+##       CONSTANTES       ##
+############################
+
+###########################
+##       VARIABLES       ##
+###########################
+
+###########################
+##       FUNCIONES       ##
+###########################
+
+###########################
+##       EJECUCIÓN       ##
+###########################
