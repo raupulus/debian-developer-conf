43c0ce9b6dcf68b1d52b2cede3418b1110df9c32
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-11 11:40:09 +0100

    Planteada estructura

--- Apps/vim.sh
+++ Apps/vim.sh
@@ -24,3 +24,59 @@
 ###########################
 ##       FUNCIONES       ##
 ###########################
+
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2017 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+
+############################
+##     IMPORTACIONES      ##
+############################
+
+###########################
+##       FUNCIONES       ##
+###########################
+vim_descargar() {
+    echo ""
+}
+
+vim_preconfiguracion() {
+    echo -e "$VE Generando Pre-Configuraciones de$RO Vim$CL"
+}
+
+vim_instalar() {
+    echo -e "$VE Preparando para instalar$RO Vim$CL"
+}
+
+vim_postconfiguracion() {
+    echo -e "$VE Generando Post-Configuraciones$RO Vim$CL"
+}
+
+vim_instalador() {
+    echo -e "$VE Comenzando instalación de$RO Vim$CL"
+
+    vim_preconfiguracion
+
+    if [[ -f '/usr/bin/vim' ]] || ; then
+        echo -e "$VE Ya esta$RO Vim$VE instalado en el equipo, omitiendo paso$CL"
+    else
+        vim_instalar
+    fi
+
+    vim_postconfiguracion
+}
