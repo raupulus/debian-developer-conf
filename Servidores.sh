909d47d2d6323738ffe7cf1784922b80b82a2a2a
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-16 01:13:06 +0100

    Rectificados colores

--- Servidores.sh
+++ Servidores.sh
@@ -41,12 +41,9 @@ CL="\e[0m"       ## Limpiar colores
 ###########################
 ##       FUNCIONES       ##
 ###########################
-DIR_SCRIPT=`echo $PWD`
-
 function server_apache() {
-
     function instalar_apache() {
-        echo -e "$verde Instalando$rojo Apache2$gris"
+        echo -e "$VE Instalando$RO Apache2$CL"
         sudo apt install -y apache2 >> /dev/null 2>> /dev/null
         sudo apt install -y libapache2-mod-perl2 >> /dev/null 2>> /dev/null
         sudo apt install -y libapache2-mod-php >> /dev/null 2>> /dev/null
@@ -54,103 +51,103 @@ function server_apache() {
     }
 
     function configurar_apache() {
-        echo -e "$verde Preparando configuracion de$rojo Apache2$gris"
+        echo -e "$VE Preparando configuracion de$RO Apache2$CL"
 
-        echo -e "$verde Activando módulos$red"
+        echo -e "$VE Activando módulos$RO"
         sudo a2enmod rewrite
 
-        echo -e "$verde Desactivando módulos$red"
+        echo -e "$VE Desactivando módulos$RO"
         sudo a2dismod php5
     }
 
     function personalizar_apache() {
         clear
-        echo -e "$verde Personalizando$rojo Apache2$gris"
+        echo -e "$VE Personalizando$RO Apache2$CL"
         function generar_www() {
             mi_usuario=`whoami`
 
             # Borrar contenido de /var/www
             sudo systemctl stop apache2
-            echo -e "$verde Cuidado, esto puede$rojo BORRAR$verde algo valioso"
+            echo -e "$VE Cuidado, esto puede$RO BORRAR$VE algo valioso"
             read -p " ¿Quieres borrar todo el directorio /var/www/? s/N → " input
             if [ $input = 's' ] || [ $input = 'S' ]
             then
                 sudo rm -R /var/www/*
             else
-                echo -e "$verde No se borra /var/www$amarillo"
+                echo -e "$VE No se borra /var/www$AM"
             fi
 
             # Copia todo el contenido WEB a /var/www
-            echo -e "$verde Copiando contenido dentro de /var/www"
+            echo -e "$VE Copiando contenido dentro de /var/www"
             sudo cp -R ./Apache2/www/* /var/www/
 
             # Copia todo el contenido de configuración a /etc/apache2
-            echo -e "$verde Copiando archivos de configuración dentro de /etc/apache2"
+            echo -e "$VE Copiando archivos de configuración dentro de /etc/apache2"
             sudo cp -R ./Apache2/etc/apache2/* /etc/apache2/
 
             # Crear archivo de usuario con permisos para directorios restringidos
-            echo -e "$verde Creando usuario con permisos en apache"
+            echo -e "$VE Creando usuario con permisos en apache"
             sudo rm /var/www/.htpasswd 2>> /dev/null
             while [ -z $input_user ]
             do
                 read -p "Nombre de usuario para acceder al sitio web privado → " input_user
             done
-            echo -e "$verde Introduce la contraseña para el sitio privado:$rojo"
+            echo -e "$VE Introduce la contraseña para el sitio privado:$RO"
             sudo htpasswd -c /var/www/.htpasswd $input_user
 
             # Cambia el dueño
-            echo -e "$verde Asignando dueños$gris"
+            echo -e "$VE Asignando dueños$CL"
             sudo chown www-data:www-data -R /var/www
             sudo chown root:root /etc/apache2/ports.conf
 
             # Agrega el usuario al grupo www-data
-            echo -e "$verde Añadiendo el usuario al grupo$rojo www-data"
+            echo -e "$VE Añadiendo el usuario al grupo$RO www-data"
             sudo adduser $mi_usuario www-data
         }
 
-        echo -e "$verde Es posible generar una estructura dentro de /var/www"
-        echo -e "$verde Ten en cuenta que esto borrará el contenido actual"
-        echo -e "$verde También se modificarán archivos en /etc/apache2/*$red"
+        echo -e "$VE Es posible generar una estructura dentro de /var/www"
+        echo -e "$VE Ten en cuenta que esto borrará el contenido actual"
+        echo -e "$VE También se modificarán archivos en /etc/apache2/*$RO"
         read -p " ¿Quieres Generar la estructura y habilitarla? s/N → " input
         if [ $input = 's' ] || [ $input = 'S' ]
         then
             generar_www
         else
-            echo -e "$verde No se genera la estructura predefinida y automática"
+            echo -e "$VE No se genera la estructura predefinida y automática"
         fi
 
         # Generar enlaces (desde ~/web a /var/www)
         function enlaces() {
             clear
-            echo -e "$verde Puedes generar un enlace en tu home ~/web hacia /var/www/html"
+            echo -e "$VE Puedes generar un enlace en tu home ~/web hacia /var/www/html"
             read -p " ¿Quieres generar el enlace? s/N → " input
             if [ $input = 's' ] || [ $input = 'S' ]
             then
                 sudo ln -s /var/www/html/ /home/$mi_usuario/web
                 sudo chown -R $mi_usuario:www-data /home/$mi_usuario/web
             else
-                echo -e "$verde No se crea enlace desde ~/web a /var/www/html"
+                echo -e "$VE No se crea enlace desde ~/web a /var/www/html"
             fi
 
             clear
-            echo -e "$verde Puedes crear un directorio para repositorios GIT en tu directorio personal"
-            echo -e "$verde Una vez creado se añadirá un enlace al servidor web"
-            echo -e "$verde Este será desde el servidor /var/www/html/Publico/GIT a ~/GIT$rojo"
+            echo -e "$VE Puedes crear un directorio para repositorios GIT en tu directorio personal"
+            echo -e "$VE Una vez creado se añadirá un enlace al servidor web"
+            echo -e "$VE Este será desde el servidor /var/www/html/Publico/GIT a ~/GIT$RO"
             read -p " ¿Quieres crear el directorio y generar el enlace? s/N → " input
             if [ $input = 's' ] || [ $input = 'S' ]
             then
-                mkdir ~/GIT 2>> /dev/null && echo -e "$verde Se ha creado el directorio ~/GIT" || echo -e "$verde No se ha creado el directorio ~/GIT"
+                mkdir ~/GIT 2>> /dev/null && echo -e "$VE Se ha creado el directorio ~/GIT" || echo -e "$VE No se ha creado el directorio ~/GIT"
                 sudo ln -s /home/$mi_usuario/GIT /var/www/html/Publico/GIT
                 sudo chown -R $mi_usuario:www-data /home/$mi_usuario/GIT
             else
-                echo -e "$verde No se crea enlaces ni directorio ~/GIT"
+                echo -e "$VE No se crea enlaces ni directorio ~/GIT"
             fi
         }
 
         enlaces
 
         # Cambia los permisos
-        echo -e "$verde Asignando permisos"
+        echo -e "$VE Asignando permisos"
         sudo chmod 775 -R /var/www/*
         sudo chmod 700 /var/www/.htpasswd
         sudo chmod 700 /var/www/html/Privado/.htaccess
@@ -168,7 +165,7 @@ function server_apache() {
         sudo a2dissite 000-default.conf
 
         function activar_hosts() {
-            echo -e "$verde Añadiendo Sitios Virtuales$amarillo"
+            echo -e "$VE Añadiendo Sitios Virtuales$AM"
             echo "127.0.0.1 privado" | sudo tee -a /etc/hosts
             echo "127.0.0.1 privado.local" | sudo tee -a /etc/hosts
             echo "127.0.0.1 p.local" | sudo tee -a /etc/hosts
@@ -181,7 +178,7 @@ function server_apache() {
         then
             activar_hosts
         else
-            echo -e "$verde No se añade nada a /etc/hosts"
+            echo -e "$VE No se añade nada a /etc/hosts"
         fi
     }
 
@@ -198,84 +195,84 @@ function server_php() {
         V_PHP="7.0"  # Versión de PHP instalada en el sistema
 
     function instalar_php() {
-        echo -e "$verde Instalando PHP$gris"
+        echo -e "$VE Instalando PHP$CL"
         paquetes_basicos="php php-cli libapache2-mod-php"
         sudo apt install -y $paquetes_basicos >> /dev/null 2>> /dev/null
 
-        echo -e "$verde Instalando paquetes extras$yellow"
+        echo -e "$VE Instalando paquetes extras$AM"
         paquetes_extras="php-gd php-curl php-pgsql php-sqlite3 sqlite sqlite3 php-intl php-mbstring php-xml php-xdebug php-json"
         sudo apt install -y $paquetes_extras >> /dev/null 2>> /dev/null
 
-        echo -e "$verde Instalando librerías$yellow"
+        echo -e "$VE Instalando librerías$AM"
         sudo apt install -y composer >> /dev/null 2>> /dev/null
         #composer global require --prefer-dist friendsofphp/php-cs-fixer squizlabs/php_codesniffer yiisoft/yii2-coding-standards phpmd/phpmd
     }
 
     function configurar_php() {
-        echo -e "$verde Preparando configuracion de PHP$gris"
+        echo -e "$VE Preparando configuracion de PHP$CL"
         PHPINI="/etc/php/$V_PHP/apache2/php.ini"  # Ruta al archivo de configuración de PHP con apache2
 
         # Modificar configuración
-        echo -e "$verde Estableciendo zona horaria por defecto para PHP$gris"
+        echo -e "$VE Estableciendo zona horaria por defecto para PHP$CL"
         sudo sed -r -i "s/^;?\s*date\.timezone\s*=.*$/date\.timezone = 'UTC'/" $PHPINI
 
-        echo -e "$verde Activando Reportar todos los errores → 'error_reporting'$gris"
+        echo -e "$VE Activando Reportar todos los errores → 'error_reporting'$CL"
         sudo sed -r -i "s/^;?\s*error_reporting\s*=.*$/error_reporting = E_ALL/" $PHPINI
 
-        echo -e "$verde Activando Mostrar errores → 'display_errors'$gris"
+        echo -e "$VE Activando Mostrar errores → 'display_errors'$CL"
         sudo sed -r -i "s/^;?\s*display_errors\s*=.*$/display_errors = On/" $PHPINI
 
-        echo -e "$verde Activando Mostrar errores al iniciar → 'display_startup_errors'$gris"
+        echo -e "$VE Activando Mostrar errores al iniciar → 'display_startup_errors'$CL"
         sudo sed -r -i "s/^;?\s*display_startup_errors\s*=.*$/display_startup_errors = On/" $PHPINI
 
         # Activar módulos
-        echo -e "$verde Activando módulos$gris"
+        echo -e "$VE Activando módulos$CL"
         sudo a2enmod "php$V_PHP"
         sudo phpenmod -s apache2 xdebug
 
-        echo -e "$verde Desactivando Módulos"
+        echo -e "$VE Desactivando Módulos"
         # xdebug para PHP CLI no tiene sentido y ralentiza
         sudo phpdismod -s cli xdebug
     }
 
     function personalizar_php() {
-        echo -e "$verde Personalizando PHP$gris"
+        echo -e "$VE Personalizando PHP$CL"
 
         function psysh() {
             function descargar_psysh() {
-                echo -e "$verde Descargando Intérprete$rojo PsySH$amarillo"
+                echo -e "$VE Descargando Intérprete$RO PsySH$AM"
 
                 REINTENTOS=10
                 for (( i=1; i<=$REINTENTOS; i++ ))
                 do
-                    rm $DIR_SCRIPT/TMP/psysh 2>> /dev/null
-                    wget --show-progress https://git.io/psysh -O $DIR_SCRIPT/TMP/psysh && break
+                    rm $WORKSCRIPT/TMP/psysh 2>> /dev/null
+                    wget --show-progress https://git.io/psysh -O $WORKSCRIPT/TMP/psysh && break
                 done
 
                 # Manual
                 for (( i=1; i<=$REINTENTOS; i++ ))
                 do
-                    rm $DIR_SCRIPT/TMP/php_manual.sqlite 2>> /dev/null
-                    wget --show-progress -q http://psysh.org/manual/es/php_manual.sqlite -O $DIR_SCRIPT/TMP/psysh/php_manual.sqlite && break
+                    rm $WORKSCRIPT/TMP/php_manual.sqlite 2>> /dev/null
+                    wget --show-progress -q http://psysh.org/manual/es/php_manual.sqlite -O $WORKSCRIPT/TMP/psysh/php_manual.sqlite && break
                 done
             }
 
             function instalar_psysh() {
-                echo -e "$verde Instalando Intérprete$rojo PsySH$amarillo"
-                cp $DIR_SCRIPT/TMP/psysh ~/.local/bin/psysh
+                echo -e "$VE Instalando Intérprete$RO PsySH$AM"
+                cp $WORKSCRIPT/TMP/psysh ~/.local/bin/psysh
                 chmod +x ~/.local/bin/psysh
 
                 # Manual
-                echo -e "$verde Instalando manual para$rojo PsySH$amarillo"
+                echo -e "$VE Instalando manual para$RO PsySH$AM"
                 mkdir -p ~/.local/share/psysh 2>> /dev/null
-                cp $DIR_SCRIPT/TMP/php_manual.sqlite ~/.local/share/psysh/php_manual.sqlite
+                cp $WORKSCRIPT/TMP/php_manual.sqlite ~/.local/share/psysh/php_manual.sqlite
             }
 
             if [ -f ~/.local/bin/psysh ]
             then
-                echo -e "$verde Ya esta$rojo psysh$verde instalado en el equipo, omitiendo paso$gris"
+                echo -e "$VE Ya esta$RO psysh$VE instalado en el equipo, omitiendo paso$CL"
             else
-                if [ -f $DIR_SCRIPT/TMP/psysh ]
+                if [ -f $WORKSCRIPT/TMP/psysh ]
                 then
                     instalar_psysh
                 else
@@ -286,7 +283,7 @@ function server_php() {
                 # Si falla la instalación se rellama la función tras limpiar
                 if [ ! -f ~/.local/bin/psysh ]
                 then
-                    rm -f $DIR_SCRIPT/TMP/psysh
+                    rm -f $WORKSCRIPT/TMP/psysh
                     descargar_psysh
                     instalar_psysh
                 fi
@@ -317,25 +314,25 @@ function server_php() {
 function server_sql() {
 
     function instalar_sql() {
-        echo -e "$verde Instalando PostgreSQL$gris"
+        echo -e "$VE Instalando PostgreSQL$CL"
         sudo apt install -y postgresql postgresql-client >> /dev/null 2>> /dev/null
         sudo apt install -y postgresql-contrib >> /dev/null 2>> /dev/null
         sudo apt install -y postgresql-all >> /dev/null 2>> /dev/null
     }
 
     function configurar_sql() {
-        echo -e "$verde Preparando configuracion de SQL$gris"
+        echo -e "$VE Preparando configuracion de SQL$CL"
         POSTGRESCONF="/etc/postgresql/9.6/main/postgresql.conf"  # Archivo de configuración para postgresql
 
-        echo -e "$verde Estableciendo intervalstyle = 'iso_8601'$gris"
+        echo -e "$VE Estableciendo intervalstyle = 'iso_8601'$CL"
         sudo sed -r -i "s/^\s*#?intervalstyle\s*=/intervalstyle = 'iso_8601' #/" $POSTGRESCONF
 
-        echo -e "$verde Estableciendo timezone = 'UTC'$gris"
+        echo -e "$VE Estableciendo timezone = 'UTC'$CL"
         sudo sed -r -i "s/^\s*#?timezone\s*=/timezone = 'UTC' #/" $POSTGRESCONF
     }
 
     function personalizar_sql() {
-        echo -e "$verde Personalizando SQL$gris"
+        echo -e "$VE Personalizando SQL$CL"
         #sudo -u postgres createdb basedatos #Crea la base de datos basedatos
         #sudo -u postgres createuser -P usuario #Crea el usuario usuario y pide que teclee su contraseña
     }
@@ -349,11 +346,11 @@ function server_sql() {
 }
 
 function instalar_servidores() {
-    echo -e "$verde Se actualizará primero las listas de repositorios"
+    echo -e "$VE Se actualizará primero las listas de repositorios"
     sudo apt update >> /dev/null 2>> /dev/null
     server_apache
     server_php
     server_sql
 
-    echo -e "$verde Se ha completado la instalacion, configuracion y personalizacion de servidores"
+    echo -e "$VE Se ha completado la instalacion, configuracion y personalizacion de servidores"
 }
