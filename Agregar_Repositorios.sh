3dc6533d6727c690c62221c4646988a2c0ff54cc
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-27 23:34:39 +0100

    Limpieza de código sobrante y corrección de variable color verde

--- Agregar_Repositorios.sh
+++ Agregar_Repositorios.sh
@@ -17,62 +17,41 @@
 ##     INSTRUCCIONES      ##
 ############################
 
-############################
-##       CONSTANTES       ##
-############################
-AM="\033[1;33m"  ## Color Amarillo
-AZ="\033[1;34m"  ## Color Azul
-BL="\033[1;37m"  ## Color Blanco
-CY="\033[1;36m"  ## Color Cyan
-GR="\033[0;37m"  ## Color CL
-MA="\033[1;35m"  ## Color Magenta
-RO="\033[1;31m"  ## Color RO
-VE="\033[1;32m"  ## Color VE
-CL="\e[0m"       ## Limpiar colores
-
-#############################
-##   Variables Generales   ##
-#############################
-
-############################
-##     IMPORTACIONES      ##
-############################
-
 ###########################
 ##       FUNCIONES       ##
 ###########################
 agregar_llaves() {
-    echo -e "$Ve Instalando llaves de repositorios$CL"
+    echo -e "$VE Instalando llaves de repositorios$CL"
 
     sudo apt install -y debian-keyring 2>> /dev/null
     sudo apt install -y pkg-mozilla-archive-keyring 2>> /dev/null
     sudo apt install -y deb-multimedia-keyring 2>> /dev/null
 
     ## Multisystem
-    echo -e "$Ve Agregando clave para$RO Multisystem$CL"
+    echo -e "$VE Agregando clave para$RO Multisystem$CL"
     sudo wget -q -O - http://liveusb.info/multisystem/depot/multisystem.asc | sudo apt-key add -
 
     ## Webmin
-    echo -e "$Ve Agregando clave para$RO Webmin$CL"
+    echo -e "$VE Agregando clave para$RO Webmin$CL"
     wget http://www.webmin.com/jcameron-key.asc && sudo apt-key add jcameron-key.asc
     sudo rm jcameron-key.asc
 
     ## Virtualbox Oficial
-    echo -e "$Ve Agregando clave para$RO Virtualbox$CL"
+    echo -e "$VE Agregando clave para$RO Virtualbox$CL"
     wget https://www.virtualbox.org/download/oracle_vbox_2016.asc
     sudo apt-key add oracle_vbox_2016.asc
     sudo rm oracle_vbox_2016.asc
 
     ## Docker
-    echo -e "$Ve Agregando clave para$RO Docker$CL"
+    echo -e "$VE Agregando clave para$RO Docker$CL"
     sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys F76221572C52609D
 
     ## Heroku
-    echo -e "$Ve Agregando clave para$RO Heroku$CL"
+    echo -e "$VE Agregando clave para$RO Heroku$CL"
     curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
 
     ## Mi propio repositorio en launchpad
-    echo -e "$Ve Agregando clave para$RO Fryntiz Repositorio$CL"
+    echo -e "$VE Agregando clave para$RO Fryntiz Repositorio$CL"
     gpg --keyserver keyserver.ubuntu.com --recv-key B5C6D9592512B8CD && gpg -a --export $PUBKRY | sudo apt-key add -
 
     ## Repositorio de PostgreSQL Oficial
@@ -81,22 +60,22 @@ agregar_llaves() {
 
 ## Añade Repositorios extras a Debian
 agregar_repositorios() {
-    echo -e "$Ve Actualizando repositorios por primera vez$CL"
+    echo -e "$VE Actualizando repositorios por primera vez$CL"
     sudo apt update >> /dev/null 2>> /dev/null
-    sudo apt install -y apt-transport-https >> /dev/null && echo -e "$Ve Instalado el paquete$RO apt-transport-https$CL" || echo -e "$Ve Error al instalar$RO apt-transport-https$CL"
+    sudo apt install -y apt-transport-https >> /dev/null && echo -e "$VE Instalado el paquete$RO apt-transport-https$CL" || echo -e "$VE Error al instalar$RO apt-transport-https$CL"
 
-    sudo apt install -y dirmngr >> /dev/null && echo -e "$Ve Instalado el paquete$RO dirmngr$CL" || echo -e "$Ve Error al instalar$RO dirmngr$CL"
-    echo -e "$Ve Agregando Repositorios$CL"
+    sudo apt install -y dirmngr >> /dev/null && echo -e "$VE Instalado el paquete$RO dirmngr$CL" || echo -e "$VE Error al instalar$RO dirmngr$CL"
+    echo -e "$VE Agregando Repositorios$CL"
 
     sudo cp $WORKSCRIPT/sources.list/sources.list.d/* /etc/apt/sources.list.d/ 2>> /dev/null
     sudo mv /etc/apt/sources.list /etc/apt/sources.list.BACKUP 2>> /dev/null
     sudo cp "$WORKSCRIPT/sources.list/sources.list" "$WORKSCRIPT/etc/apt/sources.list" 2>> /dev/null
-    echo -e "$Ve Repositorios Agregados$CL"
+    echo -e "$VE Repositorios Agregados$CL"
     sleep 3
 
-    echo -e "$Ve Actualizando listas de$RO repositorios$VE por segunda vez$CL"
+    echo -e "$VE Actualizando listas de$RO repositorios$VE por segunda vez$CL"
     sudo apt update >> /dev/null 2>> /dev/null
     agregar_llaves
-    echo -e "$Ve Actualizando listas de repositorios definitiva, comprueba que no hay$RO errores$CL"
+    echo -e "$VE Actualizando listas de repositorios definitiva, comprueba que no hay$RO errores$CL"
     sudo apt update
 }
