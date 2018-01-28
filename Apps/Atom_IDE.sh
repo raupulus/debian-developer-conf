1b5fd08b82503134a0a845aaa67fde255a963fcb
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-28 13:54:33 +0100

    Mejora enlaces de configuración

--- Apps/Atom_IDE.sh
+++ Apps/Atom_IDE.sh
@@ -22,19 +22,31 @@
 ###########################
 
 atom_preconfiguracion() {
-    echo ''
-    echo -e "$VE Se va a instalar$rojo Atom IDE$CL"
-    echo -e "$VE Puedes añadir configuraciones$AM"
-    read -p '¿Quieres configuraciones? s/N → ' input
+    echo -e "$VE Se va a instalar$RO Atom IDE$CL"
+    if [[! -h "$HOME/.atom/config.cson" ]]; then
+        enlazarHome '.atom/config.cson'
+    fi
+
+    if [[! -h "$HOME/.atom/keymap.cson" ]]; then
+        enlazarHome '.atom/keymap.cson'
+    fi
+
+    if [[! -h "$HOME/.atom/snippets.cson" ]]; then
+        enlazarHome '.atom/snippets.cson'
+    fi
+
+    echo -e "$VE Puedes limpiar$RO Atom Completamente$AM"
+    read -p '¿Quieres borrar toda la configuración existente? s/N → ' input
     if [[ $input = 's' ]] || [[ $input = 'S' ]]; then
         echo -e "$VE Añadiendo configuración nueva$CL"
-        enlazarHome '.atom/snippets.cson'
+        enlazarHome '.atom/config.cson'
         enlazarHome '.atom/keymap.cson'
+        enlazarHome '.atom/snippets.cson'
     fi
 }
 
 atom_postconfiguracion() {
-    echo -e "$VE Añadiendo configuraciones para$RO Atom$CL"
+    echo -e "$VE Añadiendo Post-Configuraciones para$RO Atom$CL"
 
     echo -e "$VE Deshabilitando complementos$CL"
     apm disable welcome
@@ -46,7 +58,7 @@ atom_plugins() {
     local atom=$(cat $WORKSCRIPT/Apps/Atom_Paquetes.lst)
 
     ## Si se ha instalado correctamente ATOM, instalamos sus plugins
-    echo -e "$VE Preparando instalación complementos$rojo Atom$CL"
+    echo -e "$VE Preparando instalación complementos$RO Atom$CL"
     if [[ -f '/usr/bin/atom' ]]; then
         for p in $atom; do
             ## Comprobación si existe instalado el complemento
@@ -67,16 +79,16 @@ atom_instalador() {
         instalarSoftwareDPKG "$WORKSCRIPT/tmp/atom.deb"
     }
 
+    ## Preparando configuración de Atom
+    atom_preconfiguracion
+
     ## Comprueba si está Atom instalado
     if [[ -f '/usr/bin/atom' ]]; then
         echo -e "$VE Ya esta$RO ATOM$VE instalado en el equipo, omitiendo paso$CL"
     else
-        ## Preparando configuración de Atom
-        atom_preconfiguracion
-
         ## Comprobar si está descargado o descargar
         if [[ -f "$WORKSCRIPT/tmp/atom.deb" ]]; then
-            echo -e "$VE Instalando$rojo Atom $CL"
+            echo -e "$VE Instalando$RO Atom $CL"
             instalarSoftwareDPKG "$WORKSCRIPT/tmp/atom.deb"
 
             ## Si falla la instalación se rellama la función tras limpiar
