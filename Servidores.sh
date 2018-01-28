128f2c67ec538c3b720bcefd70025f3aa15a30ca
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-28 01:10:16 +0100

    Establece configuración de php para todas las versiones

--- Servidores.sh
+++ Servidores.sh
@@ -174,26 +174,26 @@ server_apache() {
 }
 
 server_php() {
-    ## TOFIX → Autogenerar lista de versiones php instalados en el sistema
-    V_PHP="7.0"  ## Versión de PHP instalada en el sistema
-
     instalar_php() {
         echo -e "$VE Instalando$RO PHP$CL"
-        paquetes_basicos="php php-cli libapache2-mod-php"
+        local paquetes_basicos="php php-cli libapache2-mod-php"
         instalarSoftware "$paquetes_basicos"
 
         echo -e "$VE Instalando$RO paquetes extras$AM"
-        paquetes_extras="php-gd php-curl php-pgsql php-sqlite3 sqlite sqlite3 php-intl php-mbstring php-xml php-xdebug php-json"
+        local paquetes_extras="php-gd php-curl php-pgsql php-sqlite3 sqlite sqlite3 php-intl php-mbstring php-xml php-xdebug php-json"
         instalarSoftware "$paquetes_extras"
 
         echo -e "$VE Instalando librerías$AM"
         instalarSoftware composer
-        #composer global require --prefer-dist friendsofphp/php-cs-fixer squizlabs/php_codesniffer yiisoft/yii2-coding-standards phpmd/phpmd
     }
 
+    ##
+    ## Recibe la versión de PHP para configurar y modifica sus archivos
+    ## @param $1 Es la versión de php dentro de /etc/php
+    ##
     configurar_php() {
         echo -e "$VE Preparando configuracion de$RO PHP$CL"
-        local PHPINI="/etc/php/$V_PHP/apache2/php.ini"  # Ruta al archivo de configuración de PHP con apache2
+        local PHPINI="/etc/php/$1/apache2/php.ini"  # Ruta al archivo de configuración de PHP con apache2
 
         ## Modificar configuración
         echo -e "$VE Estableciendo zona horaria por defecto para PHP$CL"
@@ -207,15 +207,6 @@ server_php() {
 
         echo -e "$VE Activando Mostrar errores al iniciar → 'display_startup_errors'$CL"
         sudo sed -r -i "s/^;?\s*display_startup_errors\s*=.*$/display_startup_errors = On/" $PHPINI
-
-        ## Activar módulos
-        echo -e "$VE Activando módulos$CL"
-        sudo a2enmod "php$V_PHP"
-        sudo phpenmod -s apache2 xdebug
-
-        echo -e "$VE Desactivando Módulos"
-        ## Xdebug para PHP CLI no tiene sentido y ralentiza
-        sudo phpdismod -s cli xdebug
     }
 
     personalizar_php() {
@@ -260,20 +251,50 @@ server_php() {
     }
 
     ## Preparar archivo con parámetros para xdebug
-    echo -e "$VE Configurando$RO Xdebug$VE para PHP$CL"
-    echo 'zend_extension=xdebug.so
-    xdebug.remote_enable=1
-    xdebug.remote_host=127.0.0.1
-    #xdebug.remote_connect_back=1    # Not safe for production servers
-    xdebug.remote_port=9000
-    xdebug.remote_handler=dbgp
-    xdebug.remote_mode=req
-    xdebug.remote_autostart=true
-    ' | sudo tee "/etc/php/$V_PHP/apache2/conf.d/20-xdebug.ini"
+    xdebug() {
+        echo -e "$VE Configurando$RO Xdebug$VE para PHP$CL"
+        echo 'zend_extension=xdebug.so
+        xdebug.remote_enable=1
+        xdebug.remote_host=127.0.0.1
+        #xdebug.remote_connect_back=1    # Not safe for production servers
+        xdebug.remote_port=9000
+        xdebug.remote_handler=dbgp
+        xdebug.remote_mode=req
+        xdebug.remote_autostart=true
+        ' | sudo tee "/etc/php/$1/apache2/conf.d/20-xdebug.ini"
+    }
 
-    instalar_php
-    configurar_php
-    personalizar_php
+    ## Configura todas las versiones de php existentes
+    for V_PHP in '/etc/php/'; do
+        instalar_php "$V_PHP"
+        configurar_php "$V_PHP"
+        personalizar_php "$V_PHP"
+        xdebug "$V_PHP"
+    done
+
+
+    ## Pide introducir la versión de PHP para configurar con apache
+    while true :; do
+        echo -e "$VE Introduce la versión de$RO PHP$VE a utilizar$RO"
+        read -p "    → " input
+        for V_PHP in '/etc/php/'; do
+            if [[ $input = $V_PHP ]]; then
+                sudo a2enmod "php$V_PHP"
+                break
+            fi
+        done
+        echo -e "AM No es válida la opción, introduce correctamente un valor$CL"
+    done
+
+    ## Activar módulos
+    echo -e "$VE Activando módulos$CL"
+
+    sudo phpenmod -s apache2 xdebug
+
+    echo -e "$VE Desactivando Módulos"
+    ## Xdebug para PHP CLI no tiene sentido y ralentiza
+    sudo phpdismod -s cli xdebug
+
 
     ## Reiniciar apache2 para hacer efectivos los cambios
     reiniciarServicio apache2
