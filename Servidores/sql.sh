a81ea2fef89e2220fba3375df2d04a88774d1401
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-23 10:21:09 +0100

    Genero estructura con los pasos del instalador

--- Servidores/sql.sh
+++ Servidores/sql.sh
@@ -17,6 +17,30 @@
 ##     INSTRUCCIONES      ##
 ############################
 
-###########################
-##       FUNCIONES       ##
-###########################
+############################
+##        FUNCIONES       ##
+############################
+
+apache2_descargar() {
+    echo "$VE Descargando$RO Apache2$CL"
+}
+
+apache2_preconfiguracion() {
+    echo -e "$VE Generando Pre-Configuraciones de$RO Apache2"
+}
+
+apache2_instalar() {
+    echo -e "$VE Instalando$RO Apache2$CL"
+}
+
+apache2_postconfiguracion() {
+    echo -e "$VE Generando Post-Configuraciones de Apache2"
+}
+
+
+apache2_instalador() {
+    apache2_descargar
+    apache2_preconfiguracion
+    apache2_instalar
+    apache2_postconfiguracion
+}
