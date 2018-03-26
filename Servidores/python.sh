88a7a35443ca2ccb2f68766231e904cb7a7bf393
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-03-26 14:22:17 +0200

    Se añade parámetro que faltaba al descargar pyenv

--- Servidores/python.sh
+++ Servidores/python.sh
@@ -29,7 +29,7 @@ python_descargar() {
     echo "$VE Descargando$RO python$CL"
 
     ## Descargando pyenv para gestionar versión de python en un proyecto
-    descargarGIT 'https://github.com/pyenv/pyenv.git' "$HOME/.pyenv"
+    descargarGIT 'Pyenv' 'https://github.com/pyenv/pyenv.git' "$HOME/.pyenv"
 }
 
 python_preconfiguracion() {
