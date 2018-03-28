ee4f291809987637e82556fc84f76c3e3d8ab56d
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-03-28 19:02:19 +0200

    Mejoro plantillas para bash y para python

---
+++ conf/home/Plantillas/Genéricas/Scripts/Python2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+# @author     Raúl Caro Pastorino
+# @copyright  Copyright © 2018 Raúl Caro Pastorino
+# @license    https://wwww.gnu.org/licenses/gpl.txt
+# @email      tecnico@fryntiz.es
+# @web        www.fryntiz.es
+# @github     https://github.com/fryntiz
+# @gitlab     https://gitlab.com/fryntiz
+# @twitter    https://twitter.com/fryntiz
+
+# Guía de estilos aplicada: PEP8
+
+#######################################
+# #           Descripción           # #
+#######################################
+
+#######################################
+# #       Importar Librerías        # #
+#######################################
+import time  # Importamos la libreria time --> time.sleep
+import os  # Importamos la libreria para comandos de la consola/shell
+import random  # Genera números aleatorios --> random.randrange(1,100)
+# import nombre_libreria as nuevo_nombre_libreria
+
+#######################################
+# #             Variables           # #
+#######################################
+sleep = time.sleep
+
+#######################################
+# #             Funciones           # #
+#######################################
+
+
+def mifuncion():
+    pass
+
