719f44b8468965da91c0f420890cee74388d24a1
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-27 08:47:06 +0200

    Importo menú de personalización y configuro llamada a este

--- main.sh
+++ main.sh
@@ -53,10 +53,7 @@ source "$WORKSCRIPT/Instalar_Configuraciones.sh"
 source "$WORKSCRIPT/limpiador.sh"
 
 source "$WORKSCRIPT/Apps/0_Main.sh"
-source "$WORKSCRIPT/Personalizar/Configurar_GIT.sh"
-source "$WORKSCRIPT/Personalizar/Personalización_GTK.sh"
-source "$WORKSCRIPT/Personalizar/Tipografías.sh"
-source "$WORKSCRIPT/Personalizar/Variables_Entorno.sh"
+source "$WORKSCRIPT/Personalizar/0_Main.sh"
 source "$WORKSCRIPT/Servidores/0_Main.sh"
 
 ###########################
@@ -93,11 +90,7 @@ menuPrincipal() {
             1) agregar_repositorios;;     ## Menú de Repositorios
             2) menuAplicaciones;;         ## Menú de Aplicaciones
             3) instalar_configuraciones;; ## Menú de Configuraciones
-            4) menuPersonalizacion        ## Menú de Personalización
-               configuracion_git
-               personalizar
-               agregar_fuentes
-               instalar_variables;;
+            4) menuPersonalizacion;;      ## Menú de Personalización
             5) menuServidores;;           ## Menú de Servidores
             6) agregar_repositorios       ## Todos los pasos anteriores
                menuAplicaciones -a        ## Indica con "-a" que ejecute todas
