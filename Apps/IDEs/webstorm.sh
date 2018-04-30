c5afabd7cc1d366e6d60e83cc9f385730d535a41
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-30 16:43:31 +0200

    Planteamiento base para instalar webstorm

--- Apps/IDEs/webstorm.sh
+++ Apps/IDEs/webstorm.sh
@@ -21,3 +21,42 @@
 ############################
 ##       FUNCIONES        ##
 ############################
+
+
+webstorm_descargar() {
+    descargar 'Web' "https://download.jetbrains.com/webstorm/WebStorm-2018.1.2.tar.gz"
+}
+
+webstorm_preconfiguracion() {
+    echo -e "$VE Generando Pre-Configuraciones de$RO WebStorm$CL"
+}
+
+webstorm_instalar() {
+    echo -e "$VE Preparando para instalar$RO WebStorm$CL"
+}
+
+webstorm_postconfiguracion() {
+    echo -e "$VE Generando Post-Configuraciones$RO WebStorm$CL"
+
+    echo -e "$VE Generando acceso directo$CL"
+    cp "$WORKSCRIPT/Accesos_Directos/webstorm.desktop" "$HOME/.local/share/applications/"
+}
+
+webstorm_instalador() {
+    echo -e "$VE Comenzando instalación de$RO WebStorm$CL"
+
+    webstorm_preconfiguracion
+
+    if [[ -f '/usr/bin/webstorm' ]]; then
+        echo -e "$VE Ya esta$RO WebStorm$VE instalado en el equipo, omitiendo paso$CL"
+    else
+        if [[ -f "$WORKSCRIPT/tmp/WebStorm.deb" ]]; then
+            webstorm_instalar || rm -Rf "$WORKSCRIPT/tmp/WebStorm.deb"
+        else
+            webstorm_descargar
+            webstorm_instalar
+        fi
+    fi
+
+    webstorm_postconfiguracion
+}
