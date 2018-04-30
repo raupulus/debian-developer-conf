a9b6cea74673f6f3e0c95c6b51bbc7bfc6c4104c
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-30 03:06:10 +0200

    Planteada descarga y estructura de phpstorm

--- Apps/IDEs/phpstorm.sh
+++ Apps/IDEs/phpstorm.sh
@@ -16,8 +16,46 @@
 ############################
 ##     INSTRUCCIONES      ##
 ############################
-## Instala el IDE Webstorm
+## Instala el IDE PhpStorm
 
 ############################
 ##       FUNCIONES        ##
 ############################
+
+phpstorm_descargar() {
+    descargar 'Web' "https://download.jetbrains.com/webide/PhpStorm-2018.1.2.tar.gz"
+}
+
+phpstorm_preconfiguracion() {
+    echo -e "$VE Generando Pre-Configuraciones de$RO PhpStorm$CL"
+}
+
+phpstorm_instalar() {
+    echo -e "$VE Preparando para instalar$RO PhpStorm$CL"
+}
+
+phpstorm_postconfiguracion() {
+    echo -e "$VE Generando Post-Configuraciones$RO PhpStorm$CL"
+
+    echo -e "$VE Generando acceso directo$CL"
+    cp "$WORKSCRIPT/Accesos_Directos/phpstorm.desktop" "$HOME/.local/share/applications/"
+}
+
+phpstorm_instalador() {
+    echo -e "$VE Comenzando instalación de$RO PhpStorm$CL"
+
+    phpstorm_preconfiguracion
+
+    if [[ -f '/usr/bin/phpstorm' ]]; then
+        echo -e "$VE Ya esta$RO PhpStorm$VE instalado en el equipo, omitiendo paso$CL"
+    else
+        if [[ -f "$WORKSCRIPT/tmp/PhpStorm.deb" ]]; then
+            phpstorm_instalar || rm -Rf "$WORKSCRIPT/tmp/PhpStorm.deb"
+        else
+            phpstorm_descargar
+            phpstorm_instalar
+        fi
+    fi
+
+    phpstorm_postconfiguracion
+}
