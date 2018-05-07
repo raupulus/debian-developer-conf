398951206ef6013e10649d89f24edfe3471037a0
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-05-07 13:48:59 +0200

    Añado instalación básica

--- Personalizar/Desktops/i3.sh
+++ Personalizar/Desktops/i3.sh
@@ -13,6 +13,35 @@
 ##             Guía de estilos aplicada:
 ## @style      https://github.com/fryntiz/Bash_Style_Guide
 
-#############################
-##     INSTRUCCIONES       ##
-#############################
+############################
+##     INSTRUCCIONES      ##
+############################
+## Plantea la instalación de i3wm con las configuraciones
+
+############################
+##       FUNCIONES        ##
+############################
+i3wm_preconfiguracion() {
+    echo -e "$VE Generando Pre-Configuraciones de$RO i3wm$CL"
+}
+
+i3wm_instalar() {
+    echo -e "$VE Preparando para instalar$RO i3wm$CL"
+    sudo apt install i3-wm i3-blocks suckless-tools
+}
+
+##
+## Instalando software extra y configuraciones adicionales
+##
+i3wm_postconfiguracion() {
+    echo -e "$VE Generando Post-Configuraciones$RO i3wm$CL"
+    sudo apt install rxvt-unicode-256color compton compton-conf compton-conf-l10n nitrogen gpicview thunar ranger w3m tint2 arandr neofetch
+}
+
+i3wm_instalador() {
+    echo -e "$VE Comenzando instalación de$RO i3wm$CL"
+
+    i3wm_preconfiguracion
+    i3wm_instalar
+    i3wm_postconfiguracion
+}
