6730d6482c7744a4fd0cd28bf1065c6fcfd279b6
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-05-14 15:46:02 +0200

    Añado configuración de terminales al menú

--- Personalizar/0_Main.sh
+++ Personalizar/0_Main.sh
@@ -25,6 +25,7 @@ source "$WORKSCRIPT/Personalizar/Personalizacion_GTK.sh"
 source "$WORKSCRIPT/Personalizar/Tipografias.sh"
 source "$WORKSCRIPT/Personalizar/iconos.sh"
 source "$WORKSCRIPT/Personalizar/Variables_Entorno.sh"
+source "$WORKSCRIPT/Personalizar/Terminales.sh"
 source "$WORKSCRIPT/Personalizar/Desktops/0_Main.sh"
 
 ############################
@@ -35,10 +36,11 @@ menuPersonalizacion() {
         clear
         echo -e "$VE Instalando todas las personalizaciones$CL"
         configuracion_git
-        personalizarGTK
         agregar_fuentes
         instalar_variables
         instalar_iconos
+        personalizarGTK
+        terminales_instalador
     }
 
     ## Si la función recibe "-a" indica que instale todas
@@ -49,12 +51,13 @@ menuPersonalizacion() {
             clear
             local descripcion='Menú de Personalización del sistema
                 1) Configurar GIT
-                2) Personalizar GTK
-                3) Tipografías
-                4) Variables de Entorno
-                5) Instalar Iconos
-                6) Todos los pasos anteriores
-                7) Instalar un Desktop o Window Manager
+                2) Tipografías
+                3) Variables de Entorno
+                4) Instalar Iconos
+                5) Personalizar GTK
+                6) Configurar Terminales (Tilix y Terminator)
+                7) Todos los pasos anteriores
+                8) Instalar un Desktop o Window Manager
 
                 0) Atrás
             '
@@ -67,12 +70,13 @@ menuPersonalizacion() {
             case $entrada in
 
                 1)  configuracion_git;;        ## Configurar GIT
-                2)  personalizarGTK;;          ## Personalizar GTK
-                3)  agregar_fuentes;;          ## Tipografías
-                4)  instalar_variables;;       ## Variables de Entorno
-                5)  instalar_iconos;;          ## Iconos Personalizados
-                6)  todas_personalizaciones;;  ## Todos los pasos anteriores
-                7)  menuDesktops;;             ## Lleva al menú Escritorios
+                2)  agregar_fuentes;;          ## Tipografías
+                3)  instalar_variables;;       ## Variables de Entorno
+                4)  instalar_iconos;;          ## Iconos Personalizados
+                5)  personalizarGTK;;          ## Personalizar GTK
+                6)  terminales_instalador;;    ## Configura terminales
+                7)  todas_personalizaciones;;  ## Todos los pasos anteriores
+                8)  menuDesktops;;             ## Lleva al menú Escritorios
 
                 0)  ## SALIR
                     clear
