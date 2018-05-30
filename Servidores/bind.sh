0a2c121bae0351b972726ba877ce3f4fa61dbdd3
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-05-30 15:38:04 +0200

    Planteo estructura e instalación

--- Servidores/bind.sh
+++ Servidores/bind.sh
@@ -21,3 +21,37 @@
 ############################
 ##        FUNCIONES       ##
 ############################
+
+bind_descargar() {
+    echo -e "$VE Descargando$RO Bind 9$CL"
+}
+
+bind_preconfiguracion() {
+    echo -e "$VE Generando Pre-Configuraciones de$RO Bind 9"
+}
+
+bind_instalar() {
+    echo -e "$VE Instalando$RO Bind 9$CL"
+    local programa='bind9'
+    instalarSoftware "$programa"
+}
+
+bind_postconfiguracion() {
+    echo -e "$VE Generando Post-Configuraciones de Bind 9$CL"
+
+}
+
+bind_instalador() {
+    bind_descargar
+    bind_preconfiguracion
+    bind_instalar
+
+    echo -e "$VE ¿Quieres el configurar de forma$RO interactiva$VE Bind9?$CL"
+    read -p ' s/N → ' input
+    if [[ "$input" == 's' ]] || [[ "$input" == 'S' ]]; then
+        bind_postconfiguracion
+    fi
+
+    ## Reiniciar servidor BIND para aplicar configuración
+    reiniciarServicio bind
+}
