bc4442529de8c2e6ada7b507d80c45e9446ed7fe
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-28 01:45:20 +0200

    Creo script para terminales de root

---
+++ Root/terminales.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2018 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+
+##
+## Elegir intérprete de comandos entre los actuales instalados del sistema
+## Contempla bash y zsh, por defecto si no está zsh configurará solo para bash
+##
+root_interprete() {
+    echo -e "$VE Configurando terminal$CL"
+}
+
+##
+## Personaliza terminal tilix
+##
+root_tilix() {
+     echo -e "$VE Configurando tilix$CL"
+}
+
+root_terminales() {
+    echo -e "$VE Configurando terminales$CL"
+    root_tilix
+    root_interprete
+}
