049cc76e3bb041e4af7a165c5e2ab7125b799fa2
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-28 00:54:25 +0200

    Planteo configuración para nano

---
+++ Root/nano.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2018 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+
+##
+## Configurar editor de terminal, nano
+##
+root_nano() {
+    echo -e "$VE Configurando editor$RO nano$CL"
+}
