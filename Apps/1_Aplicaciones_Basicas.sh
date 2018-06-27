e42a232afba605b44fbcbf0f5ca7c7c5c60d5fcd
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-27 18:07:29 +0200

    Refactorizo instalación de aplicaciones básicas

--- Apps/1_Aplicaciones_Basicas.sh
+++ Apps/1_Aplicaciones_Basicas.sh
@@ -26,34 +26,11 @@
 ###########################
 aplicaciones_basicas() {
     echo -e "$VE Instalando aplicaciones básicas$CL"
-    ## Actualizar Listas
-    echo -e "$VE Actualizando listas de$RO Repositorios$VE (Paciencia)$CL"
-    sudo apt update >> /dev/null 2>> /dev/null
+    actualizarRepositorios
 
-    ## Repara errores de dependencias rotas que pudiesen haber
-    echo -e "$VE Comprobando estado del$RO Gestor de paquetes$VE (Paciencia)$CL"
-    sudo apt --fix-broken install -y >> /dev/null 2>> /dev/null
-    sudo apt install -f -y >> /dev/null 2>> /dev/null
+    repararGestorPaquetes
 
-    ## Instalando todo el software desde "Software.lst
-    echo -e "$VE Instalando Software adicional$CL"
-    ## La siguiente variable guarda toda la lista de paquetes desde DPKG
-    local lista_todos_paquetes=(${dpkg-query -W -f='${Installed-Size} ${Package}\n' | sort -n | cut -d" " -f2})
+    instalarSoftwareLista "$WORKSCRIPT/Apps/Packages/Software-Basico.lst"
 
-    ## Comprueba si el software está instalado y en caso contrario instala
-    local lista_Software=(`cat $WORKSCRIPT/Software.lst`)
-    for s in "${lista_Software[@]}"; do
-        for x in "${lista_todos_paquetes[@]}"; do
-            if [[ $s = $x ]]; then
-                echo -e "$RO $s$VE ya estaba instalado$CL"
-                break
-            else
-                instalarSoftware "$s"
-                break
-            fi
-        done
-    done
-
-    sudo apt --fix-broken install 2>> /dev/null
-    sudo apt install -f -y 2>> /dev/null
+    repararGestorPaquetes
 }
