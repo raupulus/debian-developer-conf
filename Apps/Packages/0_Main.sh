2a93cb4655b0fd91f37486c7489f15db4d873a02
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-27 17:11:43 +0200

    Corrijo fecha de creación

--- Apps/Packages/0_Main.sh
+++ Apps/Packages/0_Main.sh
@@ -2,7 +2,7 @@
 # -*- ENCODING: UTF-8 -*-
 ##
 ## @author     Raúl Caro Pastorino
-## @copyright  Copyright © 2017 Raúl Caro Pastorino
+## @copyright  Copyright © 2018 Raúl Caro Pastorino
 ## @license    https://wwww.gnu.org/licenses/gpl.txt
 ## @email      tecnico@fryntiz.es
 ## @web        www.fryntiz.es
