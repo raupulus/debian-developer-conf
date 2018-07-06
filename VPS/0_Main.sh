5504d7a8d63eb2d17d276e895f209fc2ea744d15
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-06 17:28:56 +0200

    Creo archivo para configurar VPS debian Stable

---
+++ VPS/0_Main.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2018 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        https://fryntiz.es
+## @gitlab     https://gitlab.com/fryntiz
+## @github     https://github.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+## Menú principal para configurar un VPS
+
+############################
+##     IMPORTACIONES      ##
+############################
+#source "$WORKSCRIPT/VPS/????.sh"
+
+###########################
+##       FUNCIONES       ##
+###########################
+##
+## Menú instalar todas las configuraciones de un VPS
+##
+menuVPS() {
+    stable_agregar_repositorios
+    aplicaciones_basicas
+
+    instalarSoftwareLista "$WORKSCRIPT/Apps/Packages/vps.lst"
+    instalarSoftwareLista "$WORKSCRIPT/Apps/Packages/developer.lst"
+
+    configuracion_git
+    terminales_instalador
+    instalar_variables
+
+    menuServidores -a
+    menuLenguajes -a
+
+    bashit_Instalador
+    ohmyzsh_Instalador
+    vim_Instalador
+}
