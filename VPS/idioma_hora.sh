36775106a8813c9c43a6a85e922cf77c8aed104b
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-22 22:12:43 +0200

    Comento tareas por hacer en cada paso siguiente

--- VPS/idioma_hora.sh
+++ VPS/idioma_hora.sh
@@ -4,4 +4,8 @@
 mainZone() {
     sudo dpkg-reconfigure locales
     sudo dpkg-reconfigure tzdata
+
+    ## Descomentar en /etc/locale.gen es_ES.UTF-8 UTF-8
+
+    ## meter LC_ALL=es_ES.UTF-8 en /etc/enviroment
 }
