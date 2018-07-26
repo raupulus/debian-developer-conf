5e03c8d4e7529ec83e023f458ee08821b27c917b
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-26 03:17:24 +0200

    Instalación básica de postfix

--- Servidores/postfix.sh
+++ Servidores/postfix.sh
@@ -16,7 +16,9 @@
 ############################
 ##     INSTRUCCIONES      ##
 ############################
-##
+## Instala un servidor postfix + dovecot
+## Usuario: postfix
+## Grupo: postfix
 
 ############################
 ##        FUNCIONES       ##
@@ -28,17 +30,39 @@ postfix_descargar() {
 
 postfix_preconfiguracion() {
     echo -e "$VE Generando Pre-Configuraciones de$RO postfix"
+    sudo apt purge -y exim4 exim4-base exim4-config
 }
 
 postfix_instalar() {
     echo -e "$VE Instalando$RO postfix$CL"
-    local software=''
+    local software='postfix dovecot-imapd dovecot-pop3d dovecot-common bsd-mailx'
     instalarSoftware "$software"
 }
 
 postfix_postconfiguracion() {
     echo -e "$VE Generando Post-Configuraciones de postfix$CL"
 
+    local dominio=''
+
+    ## Configuración, se guardará en /etc/postfix/main.cf
+    ## Sitio de internet
+    ## Nombre del sistema de correo: fryntiz.cloud
+
+    ## Pedir entrada de datos para esta parte
+    #sudo nano /etc/postfix/main.cf
+    ## mydomain = mail.fryntiz.cloud
+    ## myhostname = mail.fryntiz.cloud
+
+
+    ## Configurar dovecot
+    #sudo nano /etc/dovecot/conf.d/10-auth.conf
+    ## disable_plaintext_auth = no
+
+    ## Agregar dominios admitidos a /etc/hosts
+    echo "localhost $dominio" | sudo tee -a /etc/hosts
+
+    ## Reconfigurar postfix y comprobar datos
+    sudo dpkg-reconfigure postfix
 }
 
 postfix_instalador() {
@@ -48,4 +72,5 @@ postfix_instalador() {
 
     ## Reiniciar servidor postfix para aplicar configuración
     reiniciarServicio postfix
+    reiniciarServicio dovecot
 }
