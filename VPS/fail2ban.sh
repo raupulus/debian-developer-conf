7ee43b0af9cbd6276c1f319b6725afa0bc5c4bb8
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-26 02:04:22 +0200

    Planteo script para instalar y configurar fail2ban

---
+++ VPS/fail2ban.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2018 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      dev@fryntiz.es
+## @web        https://fryntiz.es
+## @gitlab     https://gitlab.com/fryntiz
+## @github     https://github.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+##
+
+############################
+##        FUNCIONES       ##
+############################
+
+fail2ban_descargar() {
+    echo -e "$VE Descargando$RO fail2ban$CL"
+}
+
+fail2ban_preconfiguracion() {
+    echo -e "$VE Generando Pre-Configuraciones de$RO fail2ban"
+}
+
+fail2ban_instalar() {
+    echo -e "$VE Instalando$RO fail2ban$CL"
+    local software=''
+    instalarSoftware "$software"
+}
+
+fail2ban_postconfiguracion() {
+    echo -e "$VE Generando Post-Configuraciones de fail2ban$CL"
+
+}
+
+fail2ban_instalador() {
+    fail2ban_descargar
+    fail2ban_preconfiguracion
+    fail2ban_instalar
+
+    ## Reiniciar servidor fail2ban para aplicar configuración
+    reiniciarServicio fail2ban
+}
