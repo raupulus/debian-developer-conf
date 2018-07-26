19276aa9c8e6028a498ce7edc550e32af404a11b
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-26 03:53:46 +0200

    Mejoro y amplio configuración de hora y zona

--- VPS/idioma_hora.sh
+++ VPS/idioma_hora.sh
@@ -2,10 +2,25 @@
 # -*- ENCODING: UTF-8 -*-
 
 mainZone() {
-    sudo dpkg-reconfigure locales
-    sudo dpkg-reconfigure tzdata
+    instalarSoftware 'locales'
+
+    local localegen='/etc/locale.gen'
 
     ## Descomentar en /etc/locale.gen es_ES.UTF-8 UTF-8
+    sudo sed -r -i "s/^#?\s*es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/" "$localegen"
 
-    ## meter LC_ALL=es_ES.UTF-8 en /etc/enviroment
+    ## Comprobar si existen las variables en /etc/environment para agregarlas
+    #echo "LC_ALL=es_ES.UTF-8" | sudo tee -a /etc/environment
+    #echo "LC_CTYPE=es_ES.UTF-8" | sudo tee -a /etc/environment
+    #echo "LC_MESSAGES=es_ES.UTF-8" | sudo tee -a /etc/environment
+
+    ## Exporto variables de idiomas
+    export LC_ALL="es_ES.UTF-8"
+    export LC_CTYPE="es_ES.UTF-8"
+    export LC_MESSAGES="es_ES.UTF-8"
+
+    ## Reconfigurando locales y zona
+    sudo dpkg-reconfigure locales
+    sudo dpkg-reconfigure tzdata
+    sudo locale-gen
 }
