88dbce564011137b8992f7681bb1da165b0957ac
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-08 11:04:05 +0200

    Actualizo comentarios y autoría

--- conf/home/Plantillas/Genéricas/Web/PHP.php
+++ conf/home/Plantillas/Genéricas/Web/PHP.php
@@ -4,6 +4,8 @@
  * @author Raúl Caro Pastorino
  * @copyright Copyright © 2018 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * Web del autor: https://fryntiz.es
  */
 ?>
 <html lang="es">
@@ -14,8 +16,8 @@
         <meta name="keywords" content="Raúl Caro Pastorino, Fryntiz"/>
         <meta name="author" content="Raúl Caro Pastorino"/>
         <link rel="shortcut icon" href="./images/favicon.png"/>
-        <link rel="stylesheet" href="./CSS/styles.css"/>
-        <script src="JS/scripts.js"></script>
+        <link rel="stylesheet" href="./css/styles.css"/>
+        <script src="js/scripts.js"></script>
     </head>
 
     <body>
