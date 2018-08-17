66fcd898be2aea4c39b1e8e88c1e5c094c986bea
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-08-17 17:33:58 +0200

    Creo script con la creación y configuración del usuario administrador

---
+++ VPS/administrador.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2018 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        https://fryntiz.es
+## @gitlab     https://gitlab.com/fryntiz
+## @github     https://github.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+## Configura un usuario administrador llamado web
+configureAdmin() {
+
+    ## Crea el usuario administrador: web
+    sudo adduser web
+    sudo usermod -a -G sudo web
+    sudo usermod -a -G www-data web
+    sudo usermod -a -G users web
+    sudo usermod -a -G web web
+    sudo usermod -a -G docker web
+    sudo usermod -a -G mongodb web
+    sudo usermod -a -G postgres web
+    sudo usermod -a -G crontab web
+    sudo usermod -a -G adm web
+
+    cat "$WORKSCRIPT/VPS/conf/.bashrc" | sudo tee '/home/web/.bashrc'
+
+    ## Instalo cliente angular para usuario administrador
+    sudo -u web npm install -g @angular/cli -unsafe-perm
+}
