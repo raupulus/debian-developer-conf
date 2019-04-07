753d97d04a68ca7b48a3f1882d8e3f9ea9c97653
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-04-07 19:25:09 +0200

    Actualizo versiones para IDE phpstorm y pycharm.

--- Apps/IDEs/pycharm_pro.sh
+++ Apps/IDEs/pycharm_pro.sh
@@ -72,7 +72,7 @@ pycharm_postconfiguracion() {
 }
 
 pycharm_pro_instalador() {
-    local version='pycharm-professional-2018.3.3'
+    local version='pycharm-professional-2019.1.1'
 
     echo -e "$VE Comenzando instalación de$RO PyCharm$CL"
 
