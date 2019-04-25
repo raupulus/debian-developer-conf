4e1bf14562c76934e716f5e7eb8a08b5b588b968
Raúl Caro Pastorino <dev@fryntiz.es>
2019-04-25 07:31:18 +0200

    Creo menú con selector de paquetes flatpak

--- Apps/flatpak.sh
+++ Apps/flatpak.sh
@@ -24,9 +24,53 @@ aplicaciones_flatpak() {
     echo -e "$VE Aplicaciones desde Flatpak$CL"
     actualizarRepositorios
 
-    repararGestorPaquetes
+    local gimp='https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref'
+    local libreoffice='https://flathub.org/repo/appstream/org.libreoffice.LibreOffice.flatpakref'
+    local natron='https://flathub.org/repo/appstream/fr.natron.Natron.flatpakref'
+    local kdenlive='https://flathub.org/repo/appstream/org.kde.kdenlive.flatpakref'
+    local krita='https://flathub.org/repo/appstream/org.kde.krita.flatpakref'
+    local blender='https://flathub.org/repo/appstream/org.blender.Blender.flatpakref'
+    local mypaint='https://flathub.org/repo/appstream/org.mypaint.MyPaint.flatpakref'
 
-    #instalarSoftwareLista "$SOFTLIST/Desarrollo/.lst"
 
-    repararGestorPaquetes
+    while true :; do
+        clear
+        local descripcion='Menú para instalar paquetes FlatPak
+            1) Gimp
+            2) LibreOffice
+            3) Natron
+            4) Kdenlive
+            5) Krita
+            6) Blender
+            7) MyPaint
+
+            0) Atrás
+        '
+        opciones "$descripcion"
+
+        echo -e "$RO"
+        read -p "    Acción → " entrada
+        echo -e "$CL"
+
+        case $entrada in
+
+            1)  instalarSoftwareFlatPak $gimp;;
+            2)  instalarSoftwareFlatPak $libreoffice;;
+            3)  instalarSoftwareFlatPak $natron;;
+            4)  instalarSoftwareFlatPak $kdenlive;;
+            5)  instalarSoftwareFlatPak $krita;;
+            6)  instalarSoftwareFlatPak $blender;;
+            7)  instalarSoftwareFlatPak $mypain;;
+
+            0)  ## SALIR
+                clear
+                echo -e "$RO Se sale del menú$CL"
+                echo ''
+                break;;
+
+            *)  ## Acción ante entrada no válida
+                echo ""
+                echo -e "             $RO ATENCIÓN: Elección no válida$CL";;
+        esac
+    done
 }
