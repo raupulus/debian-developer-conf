cf4ae0240972e9f52464b5b8f8a2997834abb71b
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-05-09 21:53:01 +0200

    Añado software a listas de fedora y creo generador de repositorios en del sistema

--- Repositorios/fedora.sh
+++ Repositorios/fedora.sh
@@ -26,5 +26,26 @@
 ##       FUNCIONES        ##
 ############################
 agregarRepositoriosFedora() {
-    echo -e '$VE Configurando Repositorios para$RO Fedora$CL'
+    echo -e "$VE Configurando Repositorios para$RO Fedora$CL"
+    sudo dnf update --refresh
+
+    ## Oficiales
+    sudo dnf install fedora-workstation-repositories
+
+    ## QownNotes
+    sudo rpm --import http://download.opensuse.org/repositories/home:/pbek:/QOwnNotes/Fedora_$(rpm -E %fedora)/repodata/repomd.xml.key
+    sudo wget http://download.opensuse.org/repositories/home:/pbek:/QOwnNotes/Fedora_$(rpm -E %fedora)/home:pbek:QOwnNotes.repo -O /etc/yum.repos.d/QOwnNotes.repo
+
+    ## Atom IDE
+    sudo dnf copr enable helber/atom
+
+    ## RPMfusion
+    sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+    ## Google Chrome
+    dnf config-manager --set-enabled google-chrome
+    dnf config-manager --set-enabled google-chrome-beta
+
+    sudo dnf update --refresh
 }
