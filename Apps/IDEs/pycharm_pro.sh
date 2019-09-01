4bdc9f1e6e5767adb0e324c6a555fe95207e0722
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-09-01 15:19:58 +0200

    Actualizo versiones de IDE's

--- Apps/IDEs/pycharm_pro.sh
+++ Apps/IDEs/pycharm_pro.sh
@@ -72,7 +72,7 @@ pycharm_postconfiguracion() {
 }
 
 pycharm_pro_instalador() {
-    local version='pycharm-professional-2019.1.1'
+    local version='pycharm-professional-2019.2.1'
 
     echo -e "$VE Comenzando instalación de$RO PyCharm$CL"
 
