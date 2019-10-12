36bc523a94e0299d7883b5da4032a2b2c1177b7f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-10-12 17:06:25 +0200

    Actualizo versión de Ninja IDE

--- Apps/IDEs/Ninja-IDE.sh
+++ Apps/IDEs/Ninja-IDE.sh
@@ -23,7 +23,8 @@
 ##       FUNCIONES        ##
 ############################
 ninjaide_descargar() {
-    descargar 'ninja-ide_2.3-2_all.deb' 'http://ftp.es.debian.org/debian/pool/main/n/ninja-ide/ninja-ide_2.3-2_all.deb'
+    ##descargar 'ninja-ide.deb' 'http://ftp.es.debian.org/debian/pool/main/n/ninja-ide/ninja-ide_2.3-2_all.deb'
+    descargar 'ninja-ide.deb' 'https://github.com/ninja-ide/ninja-ide/releases/download/v2.4/ninja-ide-2.4.deb'
 }
 
 ninjaide_preconfiguracion() {
@@ -34,7 +35,7 @@ ninjaide_preconfiguracion() {
 ninjaide_instalar() {
     echo -e "$VE Preparando para instalar$RO Ninja IDE$CL"
     instalarSoftware python-qt4 >> /dev/null 2>> /dev/null && echo -e "$VE Se ha instalado$RO python-qt4$CL" || echo -e "$VE No se ha instalado$RO python-qt4$CL"
-    instalarSoftwareDPKG "$WORKSCRIPT/tmp/ninja-ide_2.3-2_all.deb"
+    instalarSoftwareDPKG "$WORKSCRIPT/tmp/ninja-ide.deb"
 }
 
 ninjaide_postconfiguracion() {
@@ -60,7 +61,7 @@ ninjaide_instalador() {
     if [[ -f '/usr/bin/ninja-ide' ]]; then
         echo -e "$VE Ya esta$RO Ninja IDE$VE instalado en el equipo, omitiendo paso$CL"
     else
-        if [[ -f "$WORKSCRIPT/tmp/ninja-ide_2.3-2_all.deb" ]]; then
+        if [[ -f "$WORKSCRIPT/tmp/ninja-ide.deb" ]]; then
             ninjaide_instalar
         else
             ninjaide_descargar
@@ -69,7 +70,7 @@ ninjaide_instalador() {
 
         ## Si falla la instalación se rellama la función tras limpiar
         if [[ ! -f '/usr/bin/ninja-ide' ]]; then
-            rm -f "$WORKSCRIPT/tmp/ninja-ide_2.3-2_all.deb"
+            rm -f "$WORKSCRIPT/tmp/ninja-ide.deb"
             ninjaide_descargar
             ninjaide_instalar
         fi
