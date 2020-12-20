1ee160d297a55ba7cf8e55a9130d5d2bf5fe1412
Raúl Caro Pastorino <dev@fryntiz.es>
2020-12-20 19:24:26 +0100

    Creo función en archivo independiente para habilitar y deshabilitar servicios

---
+++ Personalizar/services.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2020 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      dev@fryntiz.es
+## @web        https://fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://gitlab.com/fryntiz/bash-guide-style
+
+############################
+##     INSTRUCCIONES      ##
+############################
+## Habilita y/o deshabilita servicios
+
+############################
+##       FUNCIONES        ##
+############################
+
+services_enable_disable() {
+    echo -e "$VE Habilitando servicios al iniciar$CL"
+
+    echo -e "$VE Deshabilitando servicios al iniciar$CL"
+    sudo systemctl disable apt-daily.timer
+    sudo systemctl stop apt-daily.timer
+
+    sudo systemctl disable apt-daily-upgrade.timer
+    sudo systemctl stop apt-daily-upgrade.timer
+
+    sudo systemctl disable unattended-upgrades.service
+    sudo systemctl stop unattended-upgrades.service
+
+
+}
