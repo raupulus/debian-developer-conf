ed86b2f83bbf2baed3ed0e065dd538e11f1f002e
Raúl Caro Pastorino <dev@fryntiz.es>
2021-02-04 00:12:00 +0100

    Add base script to generate global commands

---
+++ configurations/scripts.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2021 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      raul@fryntiz.dev
+## @web        https://fryntiz.es
+## @gitlab     https://gitlab.com/fryntiz
+## @github     https://github.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Applied Style Guide:
+## @style      https://gitlab.com/fryntiz/bash-guide-style
+
+############################
+##      INSTRUCTIONS      ##
+############################
+##
+
+###########################
+##       FUNCTIONS       ##
+###########################
+
+configurations_scripts() {
+    echo -e "$VE Añadiendo scripts a$RO /bin$CL"
+
+    ##
+    ## Añade dentro de /bin/ los scripts recibidos
+    ## @param  $*  String  Nombres de scripts dentro de conf/bin/
+    ##
+    addScriptToBin() {
+        for script in $*; do
+            sudo cp "{$WORKSCRIPT}/conf/bin/${script}" "/bin/${script}"
+            sudo chmod 755 -R "/bin/${script}"
+        done
+    }
+
+    addScriptToBin '??????'
+}
