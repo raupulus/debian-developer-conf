937d39c19708746f5e508c8b1252f265ff38c181
Raúl Caro Pastorino <dev@fryntiz.es>
2021-02-04 23:27:07 +0100

    Añado más scripts

---
+++ conf/bin/raspberry/rpi_camera_capture_video.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2021 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      raul@fryntiz.dev
+## @web        https://fryntiz.es
+## @gitlab     https://gitlab.com/fryntiz
+## @github     https://github.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Applied Style Guide:
+## @style      https://gitlab.com/fryntiz/bash-guide-style
+
+############################
+##      INSTRUCTIONS      ##
+############################
+##
+
+###########################
+##       FUNCTIONS       ##
+###########################
+
+DATE=$(date +"%Y-%m-%d_%H_%M")
+
+# Código del contador indicando cantidad de dígitos: %04d
+
+## Vídeo
+
+raspivid -o video.h264 -t 10000
+
+## Vídeo noche
+#raspivid -o video.h264 -t 10000 -ex night
