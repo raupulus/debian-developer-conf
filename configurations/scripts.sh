475168a9379feebe17c29e55fff43350d1f42f60
Raúl Caro Pastorino <dev@fryntiz.es>
2021-02-06 16:41:53 +0100

    Implement scripts deploy to /bin/

--- configurations/scripts.sh
+++ configurations/scripts.sh
@@ -22,12 +22,35 @@
 ##       FUNCTIONS       ##
 ###########################
 
+function configurations_scripts_raspberry() {
+    addScriptToBin 'rpi_camera_capture_image'
+    addScriptToBin 'rpi_camera_capture_video'
+    addScriptToBin 'rpi_read_temp'
+
+    # TODO → Este es .py, adaptar función para estos casos
+    #addScriptToBin 'rpi_control_fan_temp_gpio'
+}
+
+function configurations_scripts_generic() {
+    addScriptToBin 'check_apache_and_start'
+    addScriptToBin 'check_mysql_and_start'
+    addScriptToBin 'check_postgresql_and_start'
+    addScriptToBin 'gocd_laravel_link_storage'
+    addScriptToBin 'gocd_laravel_rebuild'
+    addScriptToBin 'gocd_publish_apache'
+    addScriptToBin 'php_version_select'
+    addScriptToBin 'scripts_info'
+    addScriptToBin 'telegram_send_message'
+    addScriptToBin 'telegram_send_message_watch_file'
+}
+
 configurations_scripts() {
     echo -e "$VE Añadiendo scripts a$RO /bin$CL"
 
-    addScriptToBin '??????'
+    configurations_scripts_generic
 
     if [[ "${MY_DISTRO}" = 'raspbian' ]]; then
         echo -e "$VE Añadiendo scripts de raspbian a$RO /bin$CL"
+        configurations_scripts_raspberry
     fi
 }
