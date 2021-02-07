f8031f432afe6900c0e6ad3cbde43961aca50de7
Raúl Caro Pastorino <dev@fryntiz.es>
2021-02-07 22:25:45 +0100

    Configurations after gocd install

--- servers/gocd.sh
+++ servers/gocd.sh
@@ -34,8 +34,6 @@ gocd_before_install() {
         sudo chmod ug+w -R '/mnt/artifact-storage'
         sudo chmod g+s -R '/mnt/artifact-storage'
     fi
-
-    ## TODO → Comprobar si está postgresql instalado
 }
 
 gocd_install() {
@@ -45,6 +43,41 @@ gocd_install() {
 
 gocd_after_install() {
     echo -e "$VE Generando Post-Configuraciones de$RO GoCD$CL"
+
+    echo -e "$VE Creando contraseña para el usuario$RO $USER$CL"
+    sudo htpasswd -B -c /etc/go/authentication "${USER}"
+    sudo chmod 755 /etc/go/authentication
+    sudo chown go:go /etc/go/authentication
+
+    echo -e "$VE Iniciando servidor, comprueba estado y pulsa cualquier tecla$CL"
+    sudo systemctl start go-server go-agent
+    sudo systemctl status go-*
+
+    echo ""
+    read -p "Pulsa cualquier INTRO para continuar" nullparam
+
+    echo -e "$VE Comprueba puertos abiertos http y https:$RO 8153$AM y$RO 8154$CL"
+    sudo watch netstat -plnt
+
+    echo ""
+    read -p "Pulsa cualquier INTRO para continuar" nullparam
+
+
+    echo -e "$VE Actualizando$RO certificados$CL"
+    sudo update-ca-certificates -f
+
+
+
+    Entrar a la url **/go/admin/security/auth_configs**
+    id → file_authentication
+    plugin id → Password File Authentication
+    password file path → /etc/go/authentication
+    Allow only known users to login → False
+
+
+    Cambiar el directorio de artifacts en la configuración:
+    :8153/go/admin/config/server#!artifact-management
+
 }
 
 gocd_installer() {
