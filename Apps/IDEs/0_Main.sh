7d59dd0349b7f09ec927fff81adc5025d6c97dfa
Raúl Caro Pastorino <raul@fryntiz.dev>
2024-03-10 02:57:10 +0100

    Remove brackets, atom and ninja ide from ide section

--- Apps/IDEs/0_Main.sh
+++ Apps/IDEs/0_Main.sh
@@ -21,14 +21,10 @@
 ############################
 ##     IMPORTACIONES      ##
 ############################
-source "$WORKSCRIPT/Apps/IDEs/Atom_IDE.sh"
-source "$WORKSCRIPT/Apps/IDEs/Brackets.sh"
-source "$WORKSCRIPT/Apps/IDEs/Ninja-IDE.sh"
 source "$WORKSCRIPT/Apps/IDEs/phpstorm.sh"
 source "$WORKSCRIPT/Apps/IDEs/pycharm_pro.sh"
 source "$WORKSCRIPT/Apps/IDEs/webstorm.sh"
 source "$WORKSCRIPT/Apps/IDEs/netbeans.sh"
-source "$WORKSCRIPT/Apps/IDEs/aptanastudio.sh"
 source "$WORKSCRIPT/Apps/IDEs/Arduino.sh"
 source "$WORKSCRIPT/Apps/IDEs/android_studio.sh"
 
@@ -43,10 +39,7 @@ menuIDES() {
     todos_IDES() {
         clear_screen
         echo -e "$VE Instalando todos los IDES$CL"
-        atom_instalador
-        brackets_instalador
         phpstorm_instalador
-        ninjaide_instalador
         pycharm_pro_instalador
         android_studio
     }
@@ -58,17 +51,15 @@ menuIDES() {
         while true :; do
             clear_screen
             local descripcion='Menú de aplicaciones
-                1) Atom
-                2) Brackets
-                3) PHP Storm
-                4) PyCharm Profesional
-                5) PyCharm Comunidad (No implementado)
-                6) Ninja IDE
-                7) NetBeans (No implementado)
-                8) Android Studio (Experimental)
-                9) Web Storm
-                10) Arduino
-                11) Todos los pasos anteriores
+                1) PHP Storm
+                2) PyCharm Profesional
+                3) PyCharm Comunidad (No implementado)
+                4) Android Studio (Experimental)
+                5) Web Storm
+                6) NetBeans (No implementado)
+                7) Arduino
+                    -------------------
+                8) Todos los pasos anteriores
 
                 0) Atrás
             '
@@ -79,17 +70,14 @@ menuIDES() {
             echo -e "$CL"
 
             case $entrada in
-
-                1)   atom_instalador;;      ##  Instala Atom
-                2)   brackets_instalador;;  ##  Instala Brackets
-                3)   phpstorm_instalador;;  ##  Instala PHP Storm
-                4)   pycharm_pro_instalador;;  ##  Instala Pycharm Profesional
-                #5)  ;;  ##
-                6)   ninjaide_instalador;;  ##  Instala Ninja IDE
-                8)   android_instalador; android_studio_instalador;;  ##
-                9)   webstorm_instalador;;  ##
-                10)  arduino_instalador;;  ##
-                11)  todos_IDES  ## Todos los IDES
+                1)   phpstorm_instalador;;  ##  Instala PHP Storm
+                2)   pycharm_pro_instalador;;  ##  Instala Pycharm Profesional
+                #3)  ;;  ##
+                4)   android_instalador; android_studio_instalador;;  ##
+                5)   webstorm_instalador;;  ##
+                #6)  ;;  ##
+                7)  arduino_instalador;;  ##
+                8)  todos_IDES  ## Todos los IDES
                      break;;
 
                 0)  ## SALIR
