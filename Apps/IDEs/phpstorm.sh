5d26890ebf3ffab29f4167ba75b75559b53218e7
Raúl Caro Pastorino <dev@fryntiz.es>
2026-08-17 18:22:20 +0200

    updated jetbrains ides

--- Apps/IDEs/phpstorm.sh
+++ Apps/IDEs/phpstorm.sh
@@ -75,7 +75,7 @@ phpstorm_postconfiguracion() {
 }
 
 phpstorm_instalador() {
-    local version='PhpStorm-2025.2.1'
+    local version='PhpStorm-2026.2.1'
 
     echo -e "$VE Comenzando instalación de$RO PhpStorm$CL"
 
--- Apps/IDEs/pycharm_pro.sh
+++ Apps/IDEs/pycharm_pro.sh
@@ -74,7 +74,7 @@ pycharm_postconfiguracion() {
 }
 
 pycharm_pro_instalador() {
-    local version='pycharm-professional-2025.2.1'
+    local version='pycharm-professional-2026.2.1'
 
     echo -e "$VE Comenzando instalación de$RO PyCharm$CL"
 
